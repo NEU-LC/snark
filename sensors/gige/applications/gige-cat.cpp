6de7057ceb79dd02cac976fdb33724a72d8ba4bf
cedric <cwohlleber@gmail.com>
2012-11-15 10:58:27 +1100

    do not exit after setting attributes

--- sensors/gige/applications/gige-cat.cpp
+++ sensors/gige/applications/gige-cat.cpp
@@ -39,7 +39,7 @@ int main( int argc, char** argv )
         boost::program_options::options_description description( "options" );
         description.add_options()
             ( "help,h", "display help message" )
-            ( "set", boost::program_options::value< std::string >( &setattributes ), "set camera attributes as comma-separated name-value pairs and exit" )
+            ( "set", boost::program_options::value< std::string >( &setattributes ), "set camera attributes as comma-separated name-value pairs" )
             ( "id", boost::program_options::value< unsigned int >( &id )->default_value( 0 ), "camera id; default: first available camera" )
             ( "discard", "discard frames, if cannot keep up; same as --buffer=1" )
             ( "buffer", boost::program_options::value< unsigned int >( &discard )->default_value( 0 ), "maximum buffer size before discarding frames, default: unlimited" )
@@ -92,7 +92,7 @@ int main( int argc, char** argv )
         snark::camera::gige camera( id, attributes );
         if( verbose ) { std::cerr << "gige-cat: connected to camera " << camera.id() << std::endl; }
         if( verbose ) { std::cerr << "gige-cat: total bytes per frame: " << camera.total_bytes_per_frame() << std::endl; }
-        if( !attributes.empty() ) { return 0; }
+
         if( vm.count( "list-attributes" ) )
         {
             attributes = camera.attributes(); // quick and dirty
