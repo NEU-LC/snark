34ce7a301c34dc7669606752c881468ee34cfd0b
v.vlaskine <v.vlaskine@acfr.usyd.edu.au>
2013-05-17 20:41:59 +1000

    geometry: first cut of sampling implemented; ellipse to test; sampling at polar regions: faulty, debug

---
+++ geometry/sample.h
@@ -0,0 +1,23 @@
+#ifndef ACFR_AERO_GEOMETRY_SAMPLE_H_
+#define ACFR_AERO_GEOMETRY_SAMPLE_H_
+
+#include "aero/geometry/coordinates.h"
+#include "aero/geometry/circle.h"
+#include "aero/geometry/region.h"
+
+namespace acfr { namespace aero {
+
+/// return a uniformly sampled point in given region (it gets denser near the poles)
+/// @param r region to sample on
+/// @param centre centre of a circle containing the region
+/// @param radius radius of a circle containing the region
+coordinates pretty_uniform_sample( const region& r, const coordinates& centre, double radius );
+
+/// return a uniformly sampled point in given region (it gets denser near the poles)
+/// @param r region to sample on
+/// @param c centre and radius of a circle containing the region
+coordinates pretty_uniform_sample( const region& r, const circle& c );
+
+} } // namespace acfr { namespace aero {
+
+#endif //ACFR_AERO_GEOMETRY_SAMPLE_H_
