8858ae09025fe98478d844b0f1eeac9a4ad72476
v.vlaskine <v.vlaskine@acfr.usyd.edu.au>
2013-05-20 10:08:26 +1000

    geometry/region: containing circle added

--- geometry/sample.h
+++ geometry/sample.h
@@ -18,6 +18,10 @@ coordinates pretty_uniform_sample( const region& r, const coordinates& centre, d
 /// @param c centre and radius of a circle containing the region
 coordinates pretty_uniform_sample( const region& r, const circle& c );
 
+/// return a uniformly sampled point in given region (it gets denser near the poles)
+/// @param r region to sample on
+coordinates pretty_uniform_sample( const region& r );
+
 } } // namespace acfr { namespace aero {
 
 #endif //ACFR_AERO_GEOMETRY_SAMPLE_H_
