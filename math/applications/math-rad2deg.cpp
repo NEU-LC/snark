45de627569f77eddafa385084a946330f04a072f
v.vlaskine <v.vlaskine@acfr.usyd.edu.au>
2013-05-25 17:40:35 +1000

    math-rad2deg: streaming implemented

--- math/applications/math-rad2deg.cpp
+++ math/applications/math-rad2deg.cpp
@@ -33,37 +33,91 @@
 
 #include <cmath>
 #include <iostream>
+#include <boost/array.hpp>
 #include <boost/lexical_cast.hpp>
+#include <comma/application/command_line_options.h>
+#include <comma/application/signal_flag.h>
+#include <comma/csv/stream.h>
 #include <comma/string/string.h>
+#include <comma/visiting/traits.h>
 
 static void usage()
 {
-        std::cerr << std::endl;
-        std::cerr << "convert angles in radians to degrees" << std::endl;
-        std::cerr << std::endl;
-        std::cerr << "usage: math-rad2deg <angle>,<angle>,..." << std::endl;
-        std::cerr << "       math-rad2deg <angle> <angle> ..." << std::endl;
-        std::cerr << std::endl;
-        exit( -1 );
+    std::cerr << std::endl;
+    std::cerr << "convert angles in degrees to radians" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "examples" << std::endl;
+    std::cerr << "    math-rad2deg <angle> <angle> ..." << std::endl;
+    std::cerr << "    cat values.csv | math-rad2deg --fields=,,,alpha,beta" << std::endl;
+    std::cerr << "    cat values.bin | math-rad2deg --fields=,b,a --binary=t,2d,ui" << std::endl;
+    std::cerr << std::endl;
+    exit( 1 );
 }
+
+struct Line
+{
+    enum { size = 1024 };
+    boost::array< double, size > values;
+};
+
+static const long double ratio = ( long double )( 180 ) / M_PI;
+
+namespace comma { namespace visiting {
+
+template <> struct traits< Line >
+{
+    template < typename K, typename V > static void visit( const K&, Line& p, V& v )
+    {
+        v.apply( "values", p.values );
+    }
+
+    template < typename K, typename V > static void visit( const K&, const Line& p, V& v )
+    {
+        v.apply( "values", p.values );
+    }
+};
+
+} } // namespace comma { namespace visiting {
 
 int main( int ac, char** av )
 {
-        if( ac == 1 ) { usage(); }
-        bool firstSpace = true;
-        std::cout.precision( 12 );
-        long double ratio = ( long double )180 / M_PI;
-        for( int i = 1; i < ac; ++i )
+    comma::command_line_options options( ac, av );
+    if( options.exists( "--help,-h" ) ) { usage(); }
+    std::vector< std::string > unnamed = options.unnamed( "", "--delimiter,-d,--binary,-b,--fields,-f" );
+    if( unnamed.empty() )
+    {
+        comma::csv::options csv( options );
+        if( csv.fields == "" ) { std::cerr << "math-rad2deg: please specify --fields (todo: handle default)" << std::endl; return 1; }
+        std::vector< std::string > v = comma::split( csv.fields, ',' );
+        if( v.size() > Line::size ) { std::cerr << "math-rad2deg: expected not more than 1024 --fields (todo: handle arbitrary number of fields)" << std::endl; return 1; }
+        unsigned int count = 0; // quick and dirty
+        for( std::size_t i = 0; i < v.size(); ++i )
         {
-                if( !firstSpace ) { std::cout << " "; } else { firstSpace = false; }
-                const std::vector < std::string >& v = comma::split( av[i], ',' );
-                bool firstComma = true;
-                for( unsigned int k = 0; k < v.size(); ++k )
-                {
-                        double angle = boost::lexical_cast< double >( v[k] );
-                        if( !firstComma ) { std::cout << ","; } else { firstComma = false; }
-                        std::cout << ( ratio * angle );
-                }
+            if( !v[i].empty() ) { v[i] = "values[" + boost::lexical_cast< std::string >( count++ ) + "]"; }
         }
-        return 0;
+        csv.fields = comma::join( v, ',' );
+        comma::csv::input_stream< Line > istream( std::cin, csv );
+        comma::csv::output_stream< Line > ostream( std::cout, csv );
+        comma::signal_flag is_shutdown;
+        while( !is_shutdown && std::cin.good() && !std::cin.eof() )
+        {
+            const Line* input = istream.read();
+            if( !input ) { break; }
+            Line output = *input; // quick and dirty
+            for( unsigned int i = 0; i < count; output.values[i] = ratio * output.values[i], ++i );
+            if( csv.binary() ) { ostream.binary().write( output, istream.binary().last() ); }
+            else { ostream.ascii().write( output, istream.ascii().last() ); }
+        }
+    }
+    else
+    {
+        std::cout.precision( 12 );
+        std::cout << ( ratio * boost::lexical_cast< double >( unnamed[0] ) );
+        for( unsigned int k = 1; k < unnamed.size(); ++k )
+        {
+            std::cout << " " << ( ratio * boost::lexical_cast< double >( unnamed[k] ) );
+        }
+        std::cout << std::endl;
+    }
+    return 0;
 }
