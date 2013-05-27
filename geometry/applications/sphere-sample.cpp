aeef89f57fb29c7a0674a4c39aba396cfe6ba182
v.vlaskine <v.vlaskine@acfr.usyd.edu.au>
2013-05-27 19:10:08 +1000

    geometry: sphere-sample renamed to sphere-region; sphere-sample is used for samples on the whole sphere now; uniform random sample still does not work

--- geometry/applications/sphere-sample.cpp
+++ geometry/applications/sphere-sample.cpp
@@ -1,171 +1,94 @@
-#include <fstream>
 #include <iostream>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
 #include <comma/application/command_line_options.h>
 #include <comma/application/signal_flag.h>
-#include <comma/base/exception.h>
 #include <comma/csv/stream.h>
-#include <comma/name_value/parser.h>
+#include <snark/math/range_bearing_elevation.h>
 #include "aero/geometry/sample.h"
-#include "aero/geometry/circle.h"
-#include "aero/geometry/ellipse.h"
-#include "aero/geometry/polygon.h"
-#include "aero/geometry/region.h"
 #include "aero/geometry/traits.h"
 
-#include <snark/math/range_bearing_elevation.h>
-#include <Eigen/Geometry>
-
-static void usage( bool more = false )
+static void usage( bool verbose )
 {
     std::cerr << std::endl;
-    std::cerr << "make filter or sample of points in a region on a sphere" << std::endl;
+    std::cerr << "generate a sample on the whole sphere" << std::endl;
     std::cerr << std::endl;
-    std::cerr << "usage: sphere-sample <options> [<output csv options>] > sample.csv" << std::endl;
+    std::cerr << "usage: sphere-sample <options> > sample.csv"<< std::endl;
     std::cerr << std::endl;
     std::cerr << "options" << std::endl;
-    std::cerr << "    --more-help,--long-help: more help output" << std::endl;
-    std::cerr << "    --region=<what><region options>" << std::endl;
-    std::cerr << "        <what>" << std::endl;
-    std::cerr << "            circle;<centre lat>,<centre long>,<radius>" << std::endl;
-    std::cerr << "            ellipse;<focus1 lat>,<focus1 long>,<focus2 lat>,<focus2 long>,<major axis>" << std::endl;
-    std::cerr << "            polygon;<point inside: lat>,<point inside: long>;<filename>[;<csv options>]" << std::endl;
-    std::cerr << std::endl;
-    std::cerr << "sampling options" << std::endl;
-    std::cerr << "    if no options given, take latitude,longitude points on stdin" << std::endl;
-    std::cerr << "     and output them to stdout, if they are inside of the region" << std::endl;
-    std::cerr << "    --resolution,--radius,-r=<degrees>: sample will be taken on grid of given resolution" << std::endl;
-    std::cerr << "                                        which gives slightly more uniform distribution" << std::endl;
-    std::cerr << "    --size,-s: if no --resolution given, sample size (number of random)" << std::endl;
-    std::cerr << "               if --resolution given, sample size per each cell; default: 1" << std::endl;
-    std::cerr << std::endl;
-    if( more )
+    std::cerr << "    --random: random uniform sample" << std::endl;
+    std::cerr << "    --regular: regular grid sample" << std::endl;
+    std::cerr << "    --resolution,-r=<value>: regular sample resolution in degrees" << std::endl;
+    if( verbose )
     {
         std::cerr << std::endl;
         std::cerr << "csv options" << std::endl;
         std::cerr << comma::csv::options::usage() << std::endl;
     }
     std::cerr << std::endl;
-    std::cerr << "examples" << std::endl;
-    std::cerr << "    10000 random points in given polygon and a point inside with lat/lon 5,5 in degrees, e.g:" << std::endl;
-    std::cerr << "    cat polygon.csv" << std::endl;
-    std::cerr << "    0,0" << std::endl;
-    std::cerr << "    0,10" << std::endl;
-    std::cerr << "    10,10" << std::endl;
-    std::cerr << "    20,0" << std::endl;
-    std::cerr << "    sphere-sample --region=\"polygon;5,5;polygon.csv\" --size=10000" << std::endl;
-    std::cerr << std::endl;
     exit( 1 );
 }
 
 using namespace acfr;
-
-static aero::region make_regions_( const std::vector< std::string >& regions )
-{
-    aero::region r;
-    if( regions.size() > 1 ) { COMMA_THROW( comma::exception, "sampling multiple regions: todo" ); }
-    for( std::size_t i = 0; i < regions.size(); ++i )
-    {
-        const std::vector< std::string > v = comma::split( regions[i], ';' );
-        if( v[0] == "circle" )
-        {
-            if( v.size() == 1 ) { COMMA_THROW( comma::exception, "expected circle parameters" ); }
-            r = aero::region( comma::csv::ascii< aero::circle >( "", ',', true ).get( v[1] ) );
-        }
-        else if( v[0] == "ellipse" )
-        {
-            if( v.size() == 1 ) { COMMA_THROW( comma::exception, "expected ellipse parameters" ); }
-            aero::ellipse e = comma::csv::ascii< aero::ellipse >( "", ',', true ).get( v[1] );
-            if( !e.valid() ) { COMMA_THROW( comma::exception, "invalid ellipse: " << v[1] ); }
-            r = aero::region( e );
-        }
-        else if( v[0] == "polygon" )
-        {
-            if( v.size() < 3 ) { COMMA_THROW( comma::exception, "expected polygon filename" ); }
-            comma::csv::options csv = comma::name_value::parser( "shape,point-inside,filename", ';' ).get< comma::csv::options >( regions[i] );
-            aero::coordinates point_inside = comma::csv::ascii< aero::coordinates >().get( v[1] );
-            std::ifstream ifs( csv.filename.c_str() );
-            if( !ifs.is_open() ) { COMMA_THROW( comma::exception, "polygon: failed to open file \"" << v[1] << "\"" ); }
-            comma::csv::input_stream< aero::coordinates > istream( ifs, csv );
-            std::vector< aero::coordinates > corners;
-            while( istream.ready() || ( !ifs.eof() && ifs.good() ) )
-            {
-                const aero::coordinates* c = istream.read();
-                if( !c ) { break; }
-                corners.push_back( *c );
-            }
-            ifs.close();
-            aero::polygon p( corners, point_inside );
-            r = aero::region( p );
-        }
-        else
-        {
-            COMMA_THROW( comma::exception, "expected region, got \"" << v[0] << "\"" );
-        }
-    }
-    return r;
-}
 
 int main( int ac, char** av )
 {
     try
     {
         comma::command_line_options options( ac, av );
-        if( options.exists( "--help,-h" ) ) { usage(); }
-        if( options.exists( "--more-help,--long-help" ) ) { usage( true ); }
-        const std::vector< std::string >& regions = options.values< std::string >( "--region,-r" );
-        const aero::region& r = make_regions_( regions );
-        comma::csv::options csv( options );
-        comma::csv::output_stream< aero::coordinates > ostream( std::cout, csv );
+        bool verbose = options.exists( "--verbose,-v" );
+        if( options.exists( "--help,-h" ) ) { usage( verbose ); }
         comma::signal_flag is_shutdown;
-        if( options.exists( "--resolution,--radius,-r" ) )
+        double resolution = options.value< double >( "--resolution,-r" ) * M_PI / 180;
+        comma::csv::output_stream< aero::coordinates > ostream( std::cout, comma::csv::options( options ) );
+        options.assert_mutually_exclusive( "--random,--regular" );
+        bool regular = options.exists( "--regular" );
+        bool random = options.exists( "--random" );
+        if( !regular && !random ) { std::cerr << "sphere-sample: expected sample type (--random or --regular)" << std::endl; return 1; }
+        if( regular )
         {
-            double radius = options.value< double >( "--resolution,--radius,-r" ) * M_PI / 180;
-            double half_radius = radius / 2;
-            std::size_t size = options.value< std::size_t >( "--size,-s", 1 );
-            aero::coordinates from( r.containing_circle().centre.latitude - r.containing_circle().radius + half_radius
-                                  , r.containing_circle().centre.longitude - r.containing_circle().radius + half_radius );
-            aero::coordinates to( r.containing_circle().centre.latitude + r.containing_circle().radius - half_radius
-                                , r.containing_circle().centre.longitude + r.containing_circle().radius - half_radius );
-            for( double e = from.latitude; !is_shutdown && e < to.latitude; e += radius )
+            aero::coordinates from( -M_PI / 2, -M_PI );
+            aero::coordinates to( M_PI / 2, M_PI );
+            for( aero::coordinates c( from ); c.latitude < to.latitude; c.latitude += resolution )
             {
-                for( double b = from.longitude; !is_shutdown && b < to.longitude; b += radius )
+                for( c.longitude = from.longitude; c.longitude < to.longitude; c.longitude += resolution )
                 {
-                    for( std::size_t i = 0; !is_shutdown && i < size; ++i )
-                    {
-                        const aero::coordinates& c = aero::pretty_uniform_sample( aero::coordinates( e, b ), half_radius );
-                        if( r.includes( c ) ) { ostream.write( c ); }
-                    }
+                    ostream.write( c );
                 }
             }
         }
-        else if( options.exists( "--size,-s" ) )
+        if( random )
         {
-            std::size_t size = options.value< std::size_t >( "--size,-s" );
-            for( std::size_t i = 0; !is_shutdown && i < size; ++i )
+            boost::mt19937 generator;
+            boost::uniform_real< double > distribution( 0, 1 );
+            boost::variate_generator< boost::mt19937&, boost::uniform_real< double > > r( generator, distribution );
+            aero::coordinates from( -M_PI / 2, -M_PI );
+            aero::coordinates to( M_PI / 2, M_PI );
+            for( aero::coordinates c( from ); !is_shutdown && c.latitude < to.latitude; c.latitude += resolution )
             {
-                static const unsigned int max_attempts = 10000;
-                unsigned int attempts = max_attempts;
-                while( attempts-- )
+                double step = resolution / std::abs( std::cos( c.latitude ) );
+                std::size_t size = M_PI * 2 / step;
+                for( std::size_t i = 0; i < size; ++i )
                 {
-                    const aero::coordinates& c = aero::pretty_uniform_sample( r.containing_circle() );
-                    if( r.includes( c ) ) { ostream.write( c ); break; }
+                    ostream.write( aero::pretty_uniform_sample( aero::coordinates( c.latitude, ( r() * 2 - 1 ) * M_PI ), resolution ) );
                 }
-                if( !attempts ) { std::cerr << "sphere-sample: failed to get random sample after " << max_attempts << std::endl; return 1; }
             }
+
+
+//             for( std::size_t i = 0; i < size; ++i )
+//             {
+//                 double z = r() * 2 - 1;
+//                 double s = density * std::sqrt( 1 - z * z );
+//                 double n = M_PI * 2.0 / s;
+//                 double q =
+//                 double a = ( r() / s * 2 - 1 ) * M_PI;
+//                 snark::range_bearing_elevation rbe( Eigen::Vector3d( std::cos( a ), std::sin( a ), z ) );
+//                 ostream.write( aero::coordinates( rbe.e(), rbe.b() ) );
+//                 //snark::range_bearing_elevation rbe( Eigen::Vector3d( r() * 2 - 1, r() * 2 - 1, r() * 2 - 1 ) );
+//                 //ostream.write( aero::coordinates( rbe.e(), rbe.b() ) );
+//             }
         }
-        else
-        {
-            comma::csv::input_stream< aero::coordinates > istream( std::cin, csv );
-            while( !is_shutdown && ( istream.ready() || ( std::cin.good() && !std::cin.eof() ) ) )
-            {
-                const aero::coordinates* c = istream.read();
-                if( !c ) { break; }
-                if( !r.includes( *c ) ) { continue; }
-                if( csv.binary() ) { std::cout.write( istream.binary().last(), istream.binary().binary().format().size() ); }
-                else { std::cout << comma::join( istream.ascii().last(), csv.delimiter ) << std::endl; }
-            }
-        }
-        return 0;
     }
     catch( std::exception& ex )
     {
