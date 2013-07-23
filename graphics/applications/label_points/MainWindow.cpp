f856a1900411264e6fbb8cd815e3c05ebd31b2cc
Cedric Wohlleber <cwohlleber@gmail.com>
2013-07-23 09:47:13 +0200

    added Qt5 support

--- graphics/applications/label_points/MainWindow.cpp
+++ graphics/applications/label_points/MainWindow.cpp
@@ -36,7 +36,12 @@
 #include <boost/bind.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/lexical_cast.hpp>
+#include "qglobal.h"
+#if QT_VERSION >= 0x050000
+#include <QtWidgets>
+#else
 #include <QtGui>
+#endif
 #include <QDialog>
 #include <QLabel>
 #include <comma/base/types.h>
@@ -121,7 +126,11 @@ MainWindow::MainWindow( const std::string& title, Viewer* viewer )
     layout->setContentsMargins( 0, 0, 0, 0 );
     layout->setSpacing( 0 );
     layout->addWidget( m_fileFrame, 0, 0 );
+  #if QT_VERSION >= 0x050000
+    layout->addWidget( QWidget::createWindowContainer( viewer ), 0, 1 );
+#else
     layout->addWidget( viewer, 0, 1 );
+#endif
     layout->setColumnStretch( 0, 0 );
     layout->setColumnStretch( 1, 1 );
     frame->setLayout( layout );
