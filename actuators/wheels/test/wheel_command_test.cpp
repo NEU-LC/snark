63640abb63cad3e1d03d0b3bd0540b4e9e59abf3
v-do <vdo.code@gmail.com>
2014-01-23 16:19:06 +1100

    actuators/wheels: modified turnrate to be in radians rather than degrees

--- actuators/wheels/test/wheel_command_test.cpp
+++ actuators/wheels/test/wheel_command_test.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <gtest/gtest.h>
 #include <snark/actuators/wheels/wheel_command.h>
 
@@ -6,20 +5,14 @@ using namespace snark::wheels;
 
 TEST(wheels, wheel_command_test)
 {
-    Eigen::Matrix4d wheel_pose = (Eigen::Matrix4d()<<-0.707107,0,-0.707107,0.687035,0.707107,0,-0.707107,0.687035,0,-1,0,-0.15034,0,0,0,1).finished();
+    Eigen::Matrix4d wheel_pose = ( Eigen::Matrix4d() << -0.707107, 0, -0.707107, 0.687035, 0.707107, 0, -0.707107, 0.687035, 0, -1, 0, -0.15034, 0, 0, 0, 1 ).finished();
 
     steer_command desired;
-    desired.velocity.x()=1;
-    desired.velocity.y()=0;
-    desired.yaw=10;
-    wheel_command wheel_command_=compute_wheel_command(desired,wheel_pose);
+    desired.velocity.x() = 1;
+    desired.velocity.y() = 0;
+    desired.turnrate = 0.174532925;
+    wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
 
-    EXPECT_LT(fabs(wheel_command_.velocity+0.905587),1e-3);
-    EXPECT_LT(fabs(wheel_command_.yaw+52.7586),1e-3);
-}
-
-int main(int argc, char *argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    EXPECT_LT( fabs( wheel_command_.velocity + 0.905587 ), 1e-3 );
+    EXPECT_LT( fabs( wheel_command_.turnrate + 0.920811279 ), 1e-3 );
 }
--- actuators/wheels/wheel_command.h
+++ actuators/wheels/wheel_command.h
@@ -1,46 +1,65 @@
-#ifndef WHEEL_COMMANDS_H
-#define WHEEL_COMMANDS_H
-#include <vector>
-#include <Eigen/Dense>
-#include <Eigen/Geometry>
+// This file is part of snark, a generic and flexible library for robotics research
+// Copyright (c) 2011 The University of Sydney
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. All advertising materials mentioning features or use of this software
+//    must display the following acknowledgement:
+//    This product includes software developed by the The University of Sydney.
+// 4. Neither the name of the The University of Sydney nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SNARK_WHEELS_WHEEL_COMMANDS_H_
+#define SNARK_WHEELS_WHEEL_COMMANDS_H_
+
 #include <boost/math/constants/constants.hpp>
-#include <comma/base/exception.h>
+#include <Eigen/Core>
 #include <snark/math/frame_transforms.h>
-#include <snark/math/applications/frame.h>
-#include <boost/lexical_cast.hpp>
 
-namespace snark{ namespace wheels{
+namespace snark { namespace wheels {
 
-namespace boost_constants = boost::math::constants;
-
-inline double deg2rad(double theta)
-{
-    return(boost_constants::pi<double>()*theta/180);
-}
-
-inline double rad2deg(double theta)
-{
-    return(180*theta/boost_constants::pi<double>());
-}
-
-const double yaw_rate_tol = 0.1; // rad/s
+const double turnrate_tolerance = 0.1; // rad/s
 
 struct steer_command
 {
     Eigen::Vector2d velocity;
-    double yaw;
+    double turnrate; // radians
+
+    steer_command() { }
+    steer_command( const Eigen::Vector2d& velocity, const double turnrate ) : velocity( velocity ), turnrate( turnrate ) { }
+    steer_command( const double x, const double y, const double turnrate ) : velocity( x, y ), turnrate( turnrate ) { }
 };
 
 struct wheel_command
 {
     double velocity;
-    double yaw;
+    double turnrate; // radians
 };
 
-/// returned yaw angle is in degrees
-wheel_command compute_wheel_command( const steer_command& desired , Eigen::Matrix4d wheel_pose_, double wheel_offset = 0 );
+/// returned turnrate is in radians
+wheel_command compute_wheel_command( const steer_command& desired, Eigen::Matrix4d wheel_pose, double wheel_offset = 0 );
 
-}}//namespace snark{ namespace wheels{
+} } // namespace snark { namespace wheels {
 
-
-#endif
+#endif // SNARK_WHEELS_WHEEL_COMMANDS_H_
