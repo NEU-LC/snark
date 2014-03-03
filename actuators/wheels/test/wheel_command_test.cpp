9dabe290f2c269c488b15e69ebe766cf23cea55e
v-do <vdo.code@gmail.com>
2014-03-03 15:54:14 +1100

    actuators/wheels: added and cleaned up wheel command tests

--- actuators/wheels/test/wheel_command_test.cpp
+++ actuators/wheels/test/wheel_command_test.cpp
@@ -17,26 +17,127 @@ TEST(wheels, wheel_command_test1)
     EXPECT_LT( fabs( wheel_command_.turnrate + 0.920811279 ), 1e-3 );
 }
 
-TEST(wheels, wheel_command_test2)
-{
-    Eigen::Matrix4d wheel_pose = ( Eigen::Matrix4d() << 1, 0, 0, -1, 0, 0, 1, -1, 0, -1, 0, 0, 0, 0, 0, 1 ).finished();
-
-    steer_command desired;
-    desired.velocity.x() = -1;
-    desired.velocity.y() = 1;
-    desired.turnrate = 0;
-    wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
-    std::cout<<"v: "<<wheel_command_.velocity<<std::endl<<"angle: "<<wheel_command_.turnrate<<std::endl;
-}
-
-TEST(wheels, wheel_command_test3)
+TEST(wheels, wheel_command_rear_left)
 {
     Eigen::Matrix4d wheel_pose = ( Eigen::Matrix4d() << -1, 0, 0, -1, 0, 0, -1, 1, 0, -1, 0, 0, 0, 0, 0, 1 ).finished();
 
-    steer_command desired;
-    desired.velocity.x() = 0;
-    desired.velocity.y() = 0;
-    desired.turnrate = 0.1;
-    wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
-    std::cout<<"v: "<<wheel_command_.velocity<<std::endl<<"angle: "<<wheel_command_.turnrate<<std::endl;
+    std::cout << "rear left: " << std::endl << wheel_pose << std::endl;
+
+    {
+        steer_command desired( -1, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move SW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move W:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, 0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, -0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CCW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
 }
+
+TEST(wheels, wheel_command_front_left)
+{
+    Eigen::Matrix4d wheel_pose = ( Eigen::Matrix4d() << -1, 0, 0, 1, 0, 0, -1, 1, 0, -1, 0, 0, 0, 0, 0, 1 ).finished();
+
+    std::cout << "front left: " << std::endl << wheel_pose << std::endl;
+
+    {
+        steer_command desired( -1, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move SW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move W:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, 0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, -0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CCW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+}
+
+TEST(wheels, wheel_command_front_right)
+{
+    Eigen::Matrix4d wheel_pose = ( Eigen::Matrix4d() << 1, 0, 0, 1, 0, 0, 1, -1, 0, -1, 0, 0, 0, 0, 0, 1 ).finished();
+
+    std::cout << "front right: " << std::endl << wheel_pose << std::endl;
+
+    {
+        steer_command desired( -1, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move SW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move W:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, 0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, -0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CCW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+}
+
+TEST(wheels, wheel_command_rear_right)
+{
+    Eigen::Matrix4d wheel_pose = ( Eigen::Matrix4d() << 1, 0, 0, -1, 0, 0, 1, -1, 0, -1, 0, 0, 0, 0, 0, 1 ).finished();
+
+    std::cout << "rear right: " << std::endl << wheel_pose << std::endl;
+
+    {
+        steer_command desired( -1, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move SW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 1, 0 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "move W:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, 0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+
+    {
+        steer_command desired( 0, 0, -0.1 );
+        wheel_command wheel_command_ = compute_wheel_command( desired, wheel_pose );
+        std::cout << "turn CCW:  velocity: " << wheel_command_.velocity << ", angle: " << wheel_command_.turnrate << std::endl;
+    }
+}
+
