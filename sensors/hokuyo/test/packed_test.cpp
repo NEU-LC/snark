62bc78a97b44dd748e9072ac691fad86317bbdf2
dewey <dewey@acfr.usyd.edu.au>
2014-07-18 16:46:54 +1000

    sensors/hokuyo: added hokuyo message data types according to communication specifications.

---
+++ sensors/hokuyo/test/packed_test.cpp
@@ -0,0 +1,60 @@
+// This file is part of snark, a generic and flexible library for robotics research
+// Copyright (c) 2011 The University of Sydney
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. All advertising materials mentioning features or use of this software
+//    must display the following acknowledgement:
+//    This product includes software developed by the The University of Sydney.
+// 4. Neither the name of the The University of Sydney nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+#ifndef WIN32
+#include <stdlib.h>
+#endif
+
+#include <iostream>
+#include <snark/timing/time.h>
+#include <gtest/gtest.h>
+#include "../message.h"
+
+namespace hok = snark::hokuyo;
+
+
+TEST( hokuyo_packed, header )
+{
+    hok::header header;
+    hok::sequence_string msg_id;
+
+    hok::request_gd gd;
+    hok::request_md md;
+    hok::reply_gd< 1080 > gd_data;
+    hok::reply_ge< 1080 > ge_data;
+    hok::reply_md md_reponse;
+    hok::reply_md_data< 1080 > md_data;
+    hok::reply_me_data< 1080 > me_data;
+}
+
+
