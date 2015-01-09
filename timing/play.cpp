96093002fecdc91229160969ce074c3c788ca9c0
Dave Jennings <dave@acfr.usyd.edu.au>
2015-01-09 15:38:26 +1100

    Fix typo in licence
    Remove repeated "the" and ensure that capitalisation matches the
    University style guide

--- timing/play.cpp
+++ timing/play.cpp
@@ -11,8 +11,8 @@
 //    documentation and/or other materials provided with the distribution.
 // 3. All advertising materials mentioning features or use of this software
 //    must display the following acknowledgement:
-//    This product includes software developed by the The University of Sydney.
-// 4. Neither the name of the The University of Sydney nor the
+//    This product includes software developed by the University of Sydney.
+// 4. Neither the name of the University of Sydney nor the
 //    names of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written permission.
 //
