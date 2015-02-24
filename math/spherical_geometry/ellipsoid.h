9eb25c82921c4973233d2e6e0988d87df91248de
Navid P <navidp@acfr.usyd.edu.au>
2015-02-24 13:48:49 +1100

    Adding discretize circle and arc to geo-calc and ellipsoid-calc; adding new geoid namespaces and adding geoids and info to geo-calc

--- math/spherical_geometry/ellipsoid.h
+++ math/spherical_geometry/ellipsoid.h
@@ -31,31 +31,72 @@
 #define SNARK_MATH_SPHERICAL_GEOMETRY_ELLIPSOID_H_
 
 #include "./coordinates.h"
+#include <vector>
+#include <boost/optional.hpp>
 
-namespace snark { namespace spherical {
-    
+namespace snark
+{
+namespace spherical
+{
+
 struct ellipsoid
 {
     /// constructor
     ellipsoid( double major_semiaxis, double minor_semiaxis );
-    
+
     double major_semiaxis;
-    
+
     double minor_semiaxis;
-    
+
     /// return distance between from and to coordinates, using vincenty's formulae
-    double distance( const coordinates& from, const coordinates& to );
-    
+    double distance( const coordinates &from, const coordinates &to );
+
     /// return coordinates at a given distance from begin at given bearing, using vincenty's formulae
-    coordinates at( const coordinates& begin, double distance, double bearing );
-    
+    /// todo: an extensive comment no what kind of bearing is used
+    coordinates at( const coordinates &begin, double distance, double bearing );
+
+    //heading degree from north; east is positive 90
+    static double bearing( double heading )
+    {
+        //bearing standard geometric radian; ie east=0,north=pi/2,south=-pi/2
+        return ( heading * -1 ) + ( M_PI / 2 );
+    }
+
+    //great circle arc
     struct arc
     {
         coordinates begin;
         coordinates end;
     };
+
+    //small circle
+    struct circle
+    {
+        //spherical coordinates in radian
+        coordinates centre;
+        //radius is distance in metric unit
+        double radius;
+        struct arc;
+        std::vector< coordinates > discretize( ellipsoid &ellipsoid, const boost::optional< double > &resolution, const boost::optional< unsigned int > &circle_size ) const;
+    };
+
+    //circular arc
+    struct circle::arc
+    {
+        /// circle of which this is an arc
+        ellipsoid::circle circle;
+        /// begin bearing
+        double begin;
+        /// end bearing
+        double end;
+        arc() {}
+        arc( const ellipsoid::circle &circle, double begin, double end ) : circle( circle ), begin( begin ), end( end ) {}
+        std::vector< coordinates > discretize( ellipsoid &ellipsoid, const boost::optional< double > &resolution, const boost::optional< unsigned int > &circle_size ) const;
+    };
+
 };
-    
-} } // namespace snark { namespace spherical {
+
+}
+} // namespace snark { namespace spherical {
 
 #endif // SNARK_MATH_SPHERICAL_GEOMETRY_ELLIPSOID_H_
