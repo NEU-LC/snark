1ff5c5e45d44382cac8a09378fa2dbaca188b5ad
Navid P <navidp@acfr.usyd.edu.au>
2015-02-26 12:56:35 +1100

    moving great circle arc to snark

---
+++ math/spherical_geometry/great_circle.h
@@ -0,0 +1,159 @@
+#ifndef SNARK_MATH_SPHERICAL_GEOMETRY_GREAT_CIRCLE_H_
+#define SNARK_MATH_SPHERICAL_GEOMETRY_GREAT_CIRCLE_H_
+
+#include <set>
+#include <vector>
+#include <Eigen/Geometry>
+#include <boost/optional.hpp>
+#include <comma/math/compare.h>
+#include "coordinates.h"
+
+namespace snark { namespace spherical {
+
+/// great circle arc (shortest of two possible)
+/// a convenience class
+/// @todo move to snark?
+struct great_circle
+{
+    class arc
+    {
+        public:
+
+            arc()
+                : begin_coordinates_( 0, 0 )
+                , end_coordinates_( 0, 0 )
+                , begin_( 1, 0, 0 )
+                , end_( 1, 0, 0 )
+                , angle_axis_( 0, Eigen::Vector3d( 0, 1, 0 ) ) // quick and dirty
+            { }
+
+//             // back and forth conversions increases floating point error and unit test failure
+// //             arc( const coordinates& begin, const coordinates& end ) : arc( begin.to_cartesian(), end.to_cartesian() ) {}
+// //             arc( const  Eigen::Vector3d& begin, const coordinates& end ) : arc( begin, end.to_cartesian() ) {}
+// //             arc( const coordinates& begin, const Eigen::Vector3d& end ) : arc( begin.to_cartesian(), end ) {}
+//
+            arc( const Eigen::Vector3d& begin, const Eigen::Vector3d& end )
+                : begin_coordinates_( begin )
+                , end_coordinates_( end )
+                , begin_( begin )
+                , end_( end )
+                , angle_axis_( Eigen::Quaternion< double >::FromTwoVectors( begin_, end_ ) )
+            {
+                if( comma::math::less( M_PI, angle() ) ) { COMMA_THROW( comma::exception, "only arcs less than pi supported; got " << angle() << " radian for begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+                //if( equal_(begin_, end_) ) { COMMA_THROW( comma::exception, "zero length great_circle::arc is not permitted: got  begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+            }
+
+            // conversion to/from cartesian is expensive all permutations should be implemented
+            arc( const coordinates& begin, const coordinates& end )
+                : begin_coordinates_( begin )
+                , end_coordinates_( end )
+                , begin_( begin )
+                , end_( end )
+                , angle_axis_( Eigen::Quaternion< double >::FromTwoVectors( begin_, end_ ) )
+            {
+                if( comma::math::less( M_PI, angle() ) ) { COMMA_THROW( comma::exception, "only arcs less than pi supported; got " << angle() << " radian for begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+                //if( equal_(begin_, end_) ) { COMMA_THROW( comma::exception, "zero length great_circle::arc is not permitted: got  begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+            }
+
+            arc( const Eigen::Vector3d& begin, const coordinates& end )
+                : begin_coordinates_( begin )
+                , end_coordinates_( end )
+                , begin_( begin )
+                , end_( end )
+                , angle_axis_( Eigen::Quaternion< double >::FromTwoVectors( begin_, end_ ) )
+            {
+                if( comma::math::less( M_PI, angle() ) ) { COMMA_THROW( comma::exception, "only arcs less than pi supported; got " << angle() << " radian for begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+                //if( equal_(begin_, end_) ) { COMMA_THROW( comma::exception, "zero length great_circle::arc is not permitted: got  begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+            }
+
+            arc( const coordinates& begin, const Eigen::Vector3d& end )
+                : begin_coordinates_( begin )
+                , end_coordinates_( end )
+                , begin_( begin )
+                , end_( end )
+                , angle_axis_( Eigen::Quaternion< double >::FromTwoVectors( begin_, end_ ) )
+            {
+                if( comma::math::less( M_PI, angle() ) ) { COMMA_THROW( comma::exception, "only arcs less than pi supported; got " << angle() << " radian for begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+                //if( equal_(begin_, end_) ) { COMMA_THROW( comma::exception, "zero length great_circle::arc is not permitted: got  begin: " << std::string(begin_coordinates_) << " end: " << std::string(end_coordinates_)); }
+            }
+
+            /// @param angle angular offset from begin in radians along the arc towards its end
+            /// @return vector at that point
+            Eigen::Vector3d at( double angle ) const;
+
+            /// @return begin as normalized cartesian vector
+            const Eigen::Vector3d& begin() const { return begin_; }
+
+            /// @return end as normalized cartesian vector
+            const Eigen::Vector3d& end() const { return end_; }
+
+            /// @return begin as normalized cartesian vector
+            const coordinates& begin_coordinates() const { return begin_coordinates_; }
+
+            /// @return end as normalized cartesian vector
+            const coordinates& end_coordinates() const { return end_coordinates_; }
+
+            /// @return angle between begin and end
+            double angle() const { return angle_axis_.angle(); }
+
+            /// @return normal to begin-end plane as normalized cartesian vector
+            const Eigen::Vector3d& axis() const { return angle_axis_.axis(); }
+
+            /// @return normalized tangent at begin + angle as normalized cartesian vector
+            Eigen::Vector3d tangent_at( double angle ) const;
+
+            /// @return normalized tangent projection on the tangent plane in the north-east frame
+            ///         at begin + angle as normalized cartesian vector
+            Eigen::Vector2d tangent_in_navigation_frame_at( double angle ) const;
+
+            /// @return bearing in the range [-pi, pi) at given angular offset from begin
+            /// @note a convenience method, since tangent_in_navigation_frame_at()
+            ///       most likely to be enough most of the time
+            double bearing( double angle = 0 ) const;
+
+            /// @return true, if the point belongs to the arc, including ends
+            /// @note ATTENTION: currently, due to the accumulating computation error
+            ///                  the precision is only up to approximately 0.02 degree
+            bool has( const coordinates& p ) const;
+            bool has( const Eigen::Vector3d& p ) const;
+
+            /// @return true, if the point belongs to the arc, excluding ends
+            bool has_inside( const coordinates& p ) const;
+            bool has_inside( const Eigen::Vector3d& p ) const;
+
+            /// return true, if two arcs belong to the same great circle and have common points
+            bool overlaps( const arc& rhs ) const;
+
+            /// @return an arc which is the shortest path (perpendicular) to this arc from a given point
+            /// if perpendicular arc does not cross with this arc segment, path to the closest end is returned
+            arc shortest_path( const coordinates &c ) const;
+
+            /// @return the angle which this arc should rotate around its beginning point, such that its begin, end and the given coordinate c, locate on the same great_circle::arc
+            double angle_towards( const coordinates &c ) const;
+
+            operator std::string() const;
+
+            bool may_intersect( const great_circle::arc& target ) const;
+
+            /// @return intersection with an arc
+            /// @note can be only one point, since we do not consider
+            ///       arcs greater than pi
+            boost::optional< coordinates > intersection_with( const arc& rhs ) const;
+            
+            static Eigen::AngleAxis< double > angle_axis(const Eigen::Vector3d &begin, const Eigen::Vector3d &end)
+            { return Eigen::AngleAxis< double >( Eigen::Quaternion< double >::FromTwoVectors( begin, end ) ); }
+
+            std::vector< coordinates > discretize( const boost::optional< double >& resolution, const boost::optional< unsigned int >& circle_size = boost::optional< unsigned int >() ) const;
+
+        private:
+            coordinates begin_coordinates_;
+            coordinates end_coordinates_;
+            Eigen::Vector3d begin_;
+            Eigen::Vector3d end_;
+            Eigen::AngleAxis< double > angle_axis_;
+    };
+};
+
+} } // namespace snark { namespace spherical {
+
+#endif // SNARK_MATH_SPHERICAL_GEOMETRY_GREAT_CIRCLE_H_
