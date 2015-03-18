02e0673295b4d16f1d427e2c1b5ed2a0a4f77a2e
Andrey Sokolov <a.sokolov@acfr.udyd.edu.au>
2015-03-18 17:46:18 +1100

    sensors/dc1394/applications/point-grey.cpp: reads config file in the same way as fire-cat (guessing format)

--- sensors/dc1394/applications/point-grey.cpp
+++ sensors/dc1394/applications/point-grey.cpp
@@ -35,50 +35,17 @@
 #include <comma/csv/format.h>
 #include <comma/name_value/ptree.h>
 #include <comma/name_value/parser.h>
+#include <comma/name_value/serialize.h>
 #include <comma/string/string.h>
 #include <comma/packed/bits.h>
 
 static const char* name() { return "point-grey"; }
 
-// quick and dirty for now, just to tear down application/ptree.h
-template < typename C >
-C config_from_ini( const std::string& filename, const std::string& name = "", const C& defaultconfig = C() )
-{
-    boost::property_tree::ptree tree;
-    C config = defaultconfig;
-    std::ifstream file;
-    file.open( filename.c_str() );
-    if ( !file.is_open() )
-    {
-        comma::to_ptree v( tree, name );
-        comma::visiting::apply( v, config );
-        boost::property_tree::ini_parser::write_ini( filename, tree );
-    }
-    else
-    {
-        boost::property_tree::ini_parser::read_ini( filename, tree );
-        boost::property_tree::ptree::assoc_iterator it = tree.find( name );
-        if( it == tree.not_found() && !name.empty() )
-        {
-            // section not found, put default
-            comma::to_ptree v( tree, name );
-            comma::visiting::apply( v, config );
-            boost::property_tree::ini_parser::write_ini(filename, tree);
-        }
-        else
-        {
-            comma::from_ptree v( tree, name, true );
-            comma::visiting::apply( v, config );
-        }
-    }
-    return config;
-}
-
 static const uint64_t PIO_DIRECTION_ADDRESS = 0x11F8;
 static const unsigned int number_of_pins = snark::camera::dc1394::number_of_pins;
 
 void set_pin_direction( snark::camera::dc1394& camera, unsigned int pin, bool is_out = true )
-{    
+{
     static const uint32_t enable_out[number_of_pins] = { 0x0001, 0x0002, 0x0004, 0x0008 };
     uint32_t value = 0;
     camera.get_control_register( value, PIO_DIRECTION_ADDRESS );
@@ -87,7 +54,7 @@ void set_pin_direction( snark::camera::dc1394& camera, unsigned int pin, bool is
     comma::packed::reverse_bits< uint32_t >( value );
     camera.set_control_register( value, PIO_DIRECTION_ADDRESS );
 }
-    
+
 int main( int argc, char** argv )
 {
     try
@@ -102,12 +69,10 @@ int main( int argc, char** argv )
             ( "config", boost::program_options::value< std::string >( &config_string ), "configuration file for the camera or semicolon-separated name=value string, run 'fire-cat -h -v' for more details" )
             ( "pin", boost::program_options::value< unsigned int >( &pin )->default_value( 0 ), "apply operation to the specified pin (default: 0)" )
             ( "direction", boost::program_options::value< std::string >( &direction ), "set pin direction to 'in' or 'out'" );
-
         boost::program_options::variables_map vm;
         boost::program_options::store( boost::program_options::parse_command_line( argc, argv, description), vm );
         boost::program_options::parsed_options parsed = boost::program_options::command_line_parser(argc, argv).options( description ).allow_unregistered().run();
         boost::program_options::notify( vm );
-        
         if ( vm.count( "help" ) || vm.count( "verbose" ) )
         {
             std::cerr << "perform operations specific to point-grey cameras" << std::endl;
@@ -119,21 +84,23 @@ int main( int argc, char** argv )
             std::cerr << std::endl;
             return 1;
         }
-        
+        if( config_string.empty() ) { std::cerr << name() << ": --config is not given" << std::endl; return 1; }
         snark::camera::dc1394::config config;
-        if( config_string.find_first_of( '=' ) == std::string::npos ) // quick and dirty
+        bool config_from_command_line = config_string.find_first_of( '=' ) != std::string::npos; // quick and dirty
+        if( config_from_command_line )
         {
-            config = config_from_ini< snark::camera::dc1394::config >( config_string );
+            config = comma::name_value::parser( ';', '=' ).get< snark::camera::dc1394::config >( config_string );
         }
         else
         {
-            comma::name_value::parser parser( ';', '=' );
-            config = parser.get< snark::camera::dc1394::config >( config_string );
+            std::vector< std::string > v = comma::split( config_string, ':' );
+            if( v.size() > 2 ) { std::cerr << name() << ": expected --config=filename or --config=filename:xpath, got '" << config_string << "'" << std::endl; return 1; }
+            std::string filename = v[0];
+            std::string xpath = ( v.size() == 1 ) ? "" : v[1];
+            config = comma::read< snark::camera::dc1394::config >( filename, xpath.c_str() );
         }
-        
         snark::camera::dc1394 camera( config );
-        
-        if( pin >= number_of_pins ) { COMMA_THROW( comma::exception, "expected pin to be from 0 to " << number_of_pins - 1 << ", got " << pin ); }
+        if( pin >= number_of_pins ) { std::cerr <<  name() << "expected pin to be from 0 to " << number_of_pins - 1 << ", got " << pin << std::endl; return 1; }
         if( vm.count( "direction" ) )
         {
             if( direction != "in" && direction != "out" ) { std::cerr << name() << ": expected direction to be either 'in' or 'out', got " << direction << std::endl; }
