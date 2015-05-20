360d44ef56277df9250588b71833697c58a66564
Andrey Sokolov <a.sokolov@acfr.usyd.edu.au>
2015-05-20 17:55:07 +1000

    timing: added timestamped.h

---
+++ timing/timestamped.h
@@ -0,0 +1,72 @@
+// This file is part of snark, a generic and flexible library for robotics research
+// Copyright (c) 2011 The University of Sydney
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the University of Sydney nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SNARK_TIMING_TIMESTAMPED_H_
+#define SNARK_TIMING_TIMESTAMPED_H_
+
+#include <boost/date_time/posix_time/ptime.hpp>
+#include <comma/visiting/traits.h>
+
+namespace snark {
+
+template < typename T >
+struct timestamped
+{
+    boost::posix_time::ptime t;
+
+    T data;
+
+    timestamped() {}
+
+    timestamped( const boost::posix_time::ptime& t, const T& data ) : t( t ), data( data ) {}
+};
+
+} // namespace snark {
+
+namespace comma { namespace visiting {
+
+template < typename T > struct traits< snark::timestamped< T > >
+{
+    template< typename K, typename V > static void visit( const K&, snark::timestamped< T >& p, V& v )
+    {
+        v.apply( "t", p.t );
+        v.apply( "data", p.data );
+    }
+
+    template< typename K, typename V > static void visit( const K&, const snark::timestamped< T >& p, V& v )
+    {
+        v.apply( "t", p.t );
+        v.apply( "data", p.data );
+    }
+};
+
+} } // namespace comma { namespace visiting {
+
+#endif // SNARK_TIMING_TIMESTAMPED_H_
+
