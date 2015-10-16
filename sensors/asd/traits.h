18ea6bbceb517486bcc3498b88627bb9ebaf807a
Navid P <navidp@acfr.usyd.edu.au>
2015-10-16 11:27:39 +1100

    updated comments

--- sensors/asd/traits.h
+++ sensors/asd/traits.h
@@ -1,3 +1,32 @@
+// This file is part of snark, a generic and flexible library for robotics research
+// Copyright (c) 2011 The University of Sydney
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the University of Sydney nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #pragma once
 #include "commands.h"
 #include <comma/visiting/traits.h>
@@ -57,7 +86,7 @@ template < > struct traits< snark::asd::commands::optimize::reply >
         v.apply( "offset[1]", t.offset[1]() );
     }
 };
-
+/*
 template < > struct traits< snark::asd::commands::restore::reply >
 {
     template< typename K, typename V > static void visit( const K& k, const snark::asd::commands::restore::reply& t, V& v )
@@ -65,11 +94,11 @@ template < > struct traits< snark::asd::commands::restore::reply >
         traits<snark::asd::commands::reply_header>::visit(k,t.header,v);
         for(int i=0;i<snark::asd::commands::restore::entry_count;i++)
         {
-            v.apply( "entry[" + to_string(i) + "]", t.entry[i]() );
+            v.apply( "entry[" + to_string(i) + "]", t.entry[i] );
         }
         v.apply( "count", t.count() );
         v.apply( "verify", t.verify() );
     }
 };
-
+*/
 } }
