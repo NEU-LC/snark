c278d87e722d6aa9812bdbee9d0896b76ff86144
Dmitry Mikhin <d.mikhin@acfr.usyd.edu.au>
2015-11-20 16:40:38 +1100

    Two more tests for may_intersect, intersection_with: spin the pairs of arcs in latitude and check if those that intersect / do not intersect always stay the same regardless of the shift.

--- math/spherical_geometry/test/intersection_test.cpp
+++ math/spherical_geometry/test/intersection_test.cpp
@@ -44,6 +44,7 @@ TEST(geometry, may_intersect_01)
 TEST(geometry, may_intersect_02)
 {
     {
+        // spin a pair of intersecting arcs in longitude: shall always report intersection regardless of longitude shift
         bool force = true;
         great_circle::arc arc1_base( coordinates_( -10.0,10.0 ), coordinates_( 10.0,15.0 ) );
         great_circle::arc arc2_base( coordinates_( 5.0,0.0 ), coordinates_( -5.0,20.0 ) );
@@ -63,6 +64,7 @@ TEST(geometry, may_intersect_02)
 TEST(geometry, may_intersect_03)
 {
     {
+        // spin a pair of non-intersecting arcs in longitude: shall never report intersection regardless of longitude shift
         bool force = true;
         great_circle::arc arc1_base( coordinates_( -10.0,10.0 ), coordinates_( 10.0,15.0 ) );
         great_circle::arc arc2_base( coordinates_( 5.0,17.0 ), coordinates_( -5.0,37.0 ) );
@@ -71,6 +73,46 @@ TEST(geometry, may_intersect_03)
         for ( size_t shift = 0 ; shift < 400; ++shift )
         {
             coordinates offset( coordinates_( 0, shift ) );
+            great_circle::arc arc1( arc1_base.begin_coordinates() + offset, arc1_base.end_coordinates() + offset );
+            great_circle::arc arc2( arc2_base.begin_coordinates() + offset, arc2_base.end_coordinates() + offset );
+            EXPECT_FALSE( arc1.may_intersect( arc2 ) );
+            EXPECT_FALSE( arc1.intersection_with( arc2, force ) );
+        }
+    }
+}
+
+TEST(geometry, may_intersect_04)
+{
+    {
+        // spin a pair of intersecting arcs in latitude: shall always report intersection regardless of latitude shift
+        bool force = true;
+        great_circle::arc arc1_base( coordinates_( -10.0,10.0 ), coordinates_( 10.0,15.0 ) );
+        great_circle::arc arc2_base( coordinates_( 5.0,0.0 ), coordinates_( -5.0,20.0 ) );
+        EXPECT_TRUE( arc1_base.may_intersect( arc2_base ) );
+        EXPECT_TRUE( arc1_base.intersection_with( arc2_base, force ) );
+        for ( size_t shift = -180 ; shift < 180; ++shift )
+        {
+            coordinates offset( coordinates_( shift, 0 ) );
+            great_circle::arc arc1( arc1_base.begin_coordinates() + offset, arc1_base.end_coordinates() + offset );
+            great_circle::arc arc2( arc2_base.begin_coordinates() + offset, arc2_base.end_coordinates() + offset );
+            EXPECT_TRUE( arc1.may_intersect( arc2 ) );
+            EXPECT_TRUE( arc1.intersection_with( arc2, force ) );
+        }
+    }
+}
+
+TEST(geometry, may_intersect_05)
+{
+    {
+        // spin a pair of non-intersecting arcs in latitude: shall never report intersection regardless of latitude shift
+        bool force = true;
+        great_circle::arc arc1_base( coordinates_( -10.0,10.0 ), coordinates_( 10.0,15.0 ) );
+        great_circle::arc arc2_base( coordinates_( 5.0,17.0 ), coordinates_( -5.0,37.0 ) );
+        EXPECT_FALSE( arc1_base.may_intersect( arc2_base ) );
+        EXPECT_FALSE( arc1_base.intersection_with( arc2_base, force ) );
+        for ( size_t shift = -180 ; shift < 180; ++shift )
+        {
+            coordinates offset( coordinates_( shift, 0 ) );
             great_circle::arc arc1( arc1_base.begin_coordinates() + offset, arc1_base.end_coordinates() + offset );
             great_circle::arc arc2( arc2_base.begin_coordinates() + offset, arc2_base.end_coordinates() + offset );
             EXPECT_FALSE( arc1.may_intersect( arc2 ) );
