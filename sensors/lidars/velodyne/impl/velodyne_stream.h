cb5c794a73f269e0843503c405a919c5db34317b
Vsevolod Vlaskine <seva@acfr.usyd.edu.au>
2016-06-10 17:13:50 +1000

    velodyne-to-csv: plugging in velodyne puck

--- sensors/lidars/velodyne/impl/velodyne_stream.h
+++ sensors/lidars/velodyne/impl/velodyne_stream.h
@@ -34,6 +34,8 @@
 #ifndef WIN32
 #include <stdlib.h>
 #endif
+#include <boost/scoped_ptr.hpp>
+#include <comma/csv/stream.h>
 #include <snark/visiting/eigen.h>
 #include "../stream.h"
 #include "../db.h"
@@ -53,22 +55,57 @@ struct velodyne_point
     comma::uint32 scan;
 };
 
+namespace velodyne {
+
+struct calculator
+{
+    virtual ~calculator() {}
+    virtual std::pair< ::Eigen::Vector3d, ::Eigen::Vector3d > ray( unsigned int laser, double range, double angle ) const = 0;
+    virtual ::Eigen::Vector3d point( unsigned int laser, double range, double angle ) const = 0;
+    virtual double range( unsigned int laser, double range ) const = 0;
+    virtual double azimuth( unsigned int laser, double azimuth ) const = 0;
+    virtual double intensity( unsigned int laser, unsigned char intensity, double distance ) const = 0;
+};
+    
+struct db_calculator : public calculator
+{
+    const velodyne::db& db;
+    
+    db_calculator( const velodyne::db& db );
+    std::pair< ::Eigen::Vector3d, ::Eigen::Vector3d > ray( unsigned int laser, double range, double angle ) const;
+    ::Eigen::Vector3d point( unsigned int laser, double range, double angle ) const;
+    double range( unsigned int laser, double range ) const;
+    double azimuth( unsigned int laser, double azimuth ) const;
+    double intensity( unsigned int laser, unsigned char intensity, double distance ) const;
+};
+
+struct puck_calculator : public calculator
+{
+    std::pair< ::Eigen::Vector3d, ::Eigen::Vector3d > ray( unsigned int laser, double range, double angle ) const;
+    ::Eigen::Vector3d point( unsigned int laser, double range, double angle ) const;
+    double range( unsigned int laser, double range ) const;
+    double azimuth( unsigned int laser, double azimuth ) const;
+    double intensity( unsigned int laser, unsigned char intensity, double distance ) const;
+};
+
+} // namespace velodyne {
+
 /// convert stream of raw velodyne data into velodyne points
-template < typename S >
+template < typename S, typename Traits = velodyne::db_calculator >
 class velodyne_stream
 {   
 public:    
-    velodyne_stream( const velodyne::db& db
-                  , bool outputInvalidpoints
-                  , boost::optional< std::size_t > from = boost::optional< std::size_t >(), boost::optional< std::size_t > to = boost::optional< std::size_t >()
+    velodyne_stream( velodyne::calculator* calculator
+                   , bool outputInvalidpoints
+                   , boost::optional< std::size_t > from = boost::optional< std::size_t >(), boost::optional< std::size_t > to = boost::optional< std::size_t >()
                    , bool raw_intensity = false
                    , bool legacy = false, bool adjusted_time=false);
 
     template < typename P >
     velodyne_stream( const P& p
-                  , const velodyne::db& db
-                  , bool outputInvalidpoints
-                  , boost::optional< std::size_t > from = boost::optional< std::size_t >(), boost::optional< std::size_t > to = boost::optional< std::size_t >() 
+                   , velodyne::calculator* calculator
+                   , bool outputInvalidpoints
+                   , boost::optional< std::size_t > from = boost::optional< std::size_t >(), boost::optional< std::size_t > to = boost::optional< std::size_t >() 
                    , bool raw_intensity = false
                    , bool legacy = false, bool adjusted_time=false);
 
@@ -77,35 +114,35 @@ public:
 
 private:
     velodyne::stream< S > m_stream;
-    velodyne::db m_db;
+    boost::scoped_ptr< velodyne::calculator > point_calculator_;
     velodyne_point m_point;
     boost::optional< std::size_t > m_to;
     bool m_raw_intensity;
 };
 
-template < typename S >
-velodyne_stream< S >::velodyne_stream ( const velodyne::db& db, bool outputInvalidpoints
+template < typename S, typename Traits >
+velodyne_stream< S, Traits >::velodyne_stream ( velodyne::calculator* calculator, bool outputInvalidpoints
                     , boost::optional< std::size_t > from
                     , boost::optional< std::size_t > to 
                     , bool raw_intensity
                     , bool legacy, bool adjusted_time):
     m_stream( new S, outputInvalidpoints, legacy, adjusted_time ),
-    m_db( db ),
+    point_calculator_( calculator ),
     m_to( to ),
     m_raw_intensity(raw_intensity)
 {
     if( from ) { while( m_stream.scan() < *from ) { m_stream.skip_scan(); } }
 }
 
-template < typename S >
+template < typename S, typename Traits >
 template < typename P >
-velodyne_stream< S >::velodyne_stream ( const P& p, const velodyne::db& db, bool outputInvalidpoints
+velodyne_stream< S, Traits >::velodyne_stream ( const P& p, velodyne::calculator* calculator, bool outputInvalidpoints
                     , boost::optional< std::size_t > from
                     , boost::optional< std::size_t > to 
                     , bool raw_intensity
                     , bool legacy, bool adjusted_time):
     m_stream( new S( p ), outputInvalidpoints, legacy, adjusted_time ),
-    m_db( db ),
+    point_calculator_( calculator ),
     m_to( to ),
     m_raw_intensity(raw_intensity)
 {
@@ -114,8 +151,8 @@ velodyne_stream< S >::velodyne_stream ( const P& p, const velodyne::db& db, bool
 
 /// read and convert one point from the stream
 /// @return false if end of stream is reached
-template < typename S >
-bool velodyne_stream< S >::read()
+template < typename S, typename Traits >
+bool velodyne_stream< S, Traits >::read()
 {
     if( m_to && m_stream.scan() > *m_to ) { return false; }
     const velodyne::laser_return* r = m_stream.read();
@@ -123,18 +160,18 @@ bool velodyne_stream< S >::read()
     m_point.timestamp = r->timestamp;
     m_point.id = r->id;
     // multiply by 255 to keep with the old format
-    m_point.intensity = m_raw_intensity ? r->intensity : (m_db.lasers[ m_point.id ].intensity(r->intensity, r->range) * 255);
+    m_point.intensity = m_raw_intensity ? r->intensity : ( point_calculator_->intensity( m_point.id, r->intensity, r->range ) * 255);
     m_point.valid = !comma::math::equal( r->range, 0 ); // quick and dirty
-    m_point.ray = m_db.lasers[ m_point.id ].ray( r->range, r->azimuth );
-    m_point.range = m_db.lasers[ m_point.id ].range( r->range );
+    m_point.ray = point_calculator_->ray( m_point.id, r->range, r->azimuth );
+    m_point.range = point_calculator_->range( m_point.id, r->range );
     m_point.scan = m_stream.scan();
-    m_point.azimuth = m_db.lasers[ m_point.id ].azimuth( r->azimuth );
+    m_point.azimuth = point_calculator_->azimuth( m_point.id, r->azimuth );
     return true;
 }
 
 /// specialisation for csv input stream: in this case nothing to convert
-template <>
-class velodyne_stream< comma::csv::input_stream< velodyne_point> >
+template < typename Traits >
+class velodyne_stream< comma::csv::input_stream< velodyne_point >, Traits >
 {
 public:
 
@@ -150,20 +187,16 @@ private:
 
 /// read and forward one point from the stream: nothing to convert as the input is csv
 /// @return false if end of stream is reached
-inline bool velodyne_stream< comma::csv::input_stream< velodyne_point> >::read()
+template < typename Traits >
+inline bool velodyne_stream< comma::csv::input_stream< velodyne_point >, Traits >::read()
 {
     const velodyne_point* point = m_stream.read();
-    if( point == NULL )
-    {
-        return false;
-    }
+    if( !point ) { return false; }
     m_point = *point;
     return true;
 }
 
-
-} 
-
+} // namespace snark
 
 namespace comma { namespace visiting {
 
