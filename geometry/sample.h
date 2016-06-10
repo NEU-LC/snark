4a395b9eb731ef3c8b2e006735c04827e89afc66
Aneel Rathore <a.rathore@acfr.usyd.edu.au>
2016-06-10 15:00:02 +1000

    add copyright header to .cpp and .h files

--- geometry/sample.h
+++ geometry/sample.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2013-2016. This code was produced by the
+// Australian Centre for Field Robotics, The University of Sydney under
+// the Future Flight Planning project, University Reference 13996, contract
+// NSW-CPS-2011-015264, Work Orders 5, 7 and 8. The intellectual property
+// ownership is as set out in these contracts, as registered with
+// Commercial Development and Industry Partnerships.
+
 #ifndef ACFR_AERO_GEOMETRY_SAMPLE_H_
 #define ACFR_AERO_GEOMETRY_SAMPLE_H_
 
