541a153ddc4af8bba17c66bdfd80bcd4f3d54637
Dmitry Mikhin <d.mikhin@acfr.usyd.edu.au>
2016-10-28 10:58:01 +1100

    math/spherical_geometry/coordinates: more unit tests of is_near.

--- math/spherical_geometry/test/coordinates_test.cpp
+++ math/spherical_geometry/test/coordinates_test.cpp
@@ -49,6 +49,9 @@ namespace {
 
         return std::make_pair( time_near, time_far );
     }
+
+    double deg2rad( double degrees ) { return degrees / 180.0 * M_PI; }
+    double rad2deg( double radians ) { return radians * 180.0 / M_PI; }
 }
 
 TEST( coordinates, is_near_performance )
@@ -84,5 +87,86 @@ TEST( coordinates, is_near_performance )
         for ( size_t i = 0; i < points.size(); ++i ) { points[i].latitude = anchor.latitude; }
         std::pair< double, double > timings = call_is_near( anchor, points, repeat, 0.0001, M_PI, verbose );
         EXPECT_TRUE( timings.second >= 10 * timings.first );
+    }
+}
+
+TEST( coordinates, is_near_basic )
+{
+    EXPECT_TRUE(  coordinates::is_near( coordinates::from_degrees( 1.0, 1.0 ), coordinates::from_degrees( 1.0, 1.01 ), deg2rad(0.1)   ) );
+    EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 1.0, 1.0 ), coordinates::from_degrees( 1.0, 1.01 ), deg2rad(0.001) ) );
+}
+
+TEST( coordinates, is_near_across_equator )
+{
+    EXPECT_TRUE(  coordinates::is_near( coordinates::from_degrees( 0.1, 1.0 ), coordinates::from_degrees( -0.1, 1.0 ), deg2rad(0.25) ) );
+    EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 0.1, 1.0 ), coordinates::from_degrees( -0.1, 1.0 ), deg2rad(0.1)  ) );
+}
+
+TEST( coordinates, is_near_across_zero_meridian )
+{
+    EXPECT_TRUE(  coordinates::is_near( coordinates::from_degrees( 1.0,   0.1 ), coordinates::from_degrees( 1.0, -0.1 ), deg2rad(0.25) ) );
+    EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 1.0,   0.1 ), coordinates::from_degrees( 1.0, -0.1 ), deg2rad(0.1)  ) );
+    EXPECT_TRUE(  coordinates::is_near( coordinates::from_degrees( 1.0, 359.9 ), coordinates::from_degrees( 1.0,  0.1 ), deg2rad(0.25) ) );
+    EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 1.0, 359.9 ), coordinates::from_degrees( 1.0,  0.1 ), deg2rad(0.1)  ) );
+    EXPECT_TRUE(  coordinates::is_near( coordinates::from_degrees( 1.0, 360.0 ), coordinates::from_degrees( 1.0,  0.1 ), deg2rad(0.25) ) );
+    EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 1.0, 360.0 ), coordinates::from_degrees( 1.0,  0.1 ), deg2rad(0.09) ) );
+}
+
+TEST( coordinates, is_near_epsilon_above_globe )
+{
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( 10.0,  10.0 ), coordinates::from_degrees(  50.0, -20.0 ), deg2rad(180.1) ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( 60.0, 110.0 ), coordinates::from_degrees( -60.0, -70.0 ), deg2rad(180.1) ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( 90.0,   0.0 ), coordinates::from_degrees( -90.0, 180.0 ), deg2rad(180.1) ) );
+}
+
+TEST( coordinates, is_near_epsilon_half_globe )
+{
+    EXPECT_TRUE(  coordinates::is_near( coordinates::from_degrees( 10.0,  10.0 ), coordinates::from_degrees(  50.0, -20.0 ), deg2rad(179.999) ) );
+    EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 60.0, 110.0 ), coordinates::from_degrees( -60.0, -70.0 ), deg2rad(179.999) ) );
+    EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 90.0,   0.0 ), coordinates::from_degrees( -90.0, 180.0 ), deg2rad(179.999) ) );
+}
+
+TEST( coordinates, is_near_high_latitude_north )
+{
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( 85.0,  10.0 ), coordinates::from_degrees( 85.0, -20.0 ), deg2rad(5.0)  ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( 90.0, 110.0 ), coordinates::from_degrees( 85.0, -70.0 ), deg2rad(5.01) ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( 89.0,   0.0 ), coordinates::from_degrees( 89.0, 180.0 ), deg2rad(5.0)  ) );
+}
+
+TEST( coordinates, is_near_high_latitude_south )
+{
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( -85.0,  10.0 ), coordinates::from_degrees( -85.0, -20.0 ), deg2rad(5.0)  ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( -90.0, 110.0 ), coordinates::from_degrees( -85.0, -70.0 ), deg2rad(5.01) ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( -89.0,   0.0 ), coordinates::from_degrees( -89.0, 180.0 ), deg2rad(5.0)  ) );
+}
+
+TEST( coordinates, is_near_80_latitude )
+{
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees(  80.1,  10.0 ), coordinates::from_degrees(  79.9,  10.0 ), deg2rad(0.5) ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( -80.1, 110.0 ), coordinates::from_degrees( -79.9, 110.1 ), deg2rad(0.5) ) );
+    EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( -80.1,  -0.1 ), coordinates::from_degrees( -79.9, 360.1 ), deg2rad(0.5) ) );
+}
+
+// NOTE: this test relies on the broken, numerically unstable implementation of angle calculations (third, precise branch of is_far)
+// the test would start to fail once the problem is fixed; fix the test then, do not try to rid the implementation
+TEST( coordinates, is_near_broken_precision )
+{
+    // enforce the 3rd branch of is_far that relies on unstable computations
+    {
+        double tiny_distance = rad2deg( 0.5 / 6371000.0 );   // 0.5 m at the Earth radius; still not zero
+        double tiny_epsilon = deg2rad(0.5 * tiny_distance);
+        EXPECT_TRUE( tiny_epsilon < coordinates::epsilon );
+        // epsilon is smaller then distance, cannot be near; but angle is zero and is reported as near
+        EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( tiny_distance, 0.0 ), coordinates::from_degrees( 0.0, 0.0 ), tiny_epsilon ) );
+        EXPECT_TRUE( coordinates::is_near( coordinates::from_degrees( 0.0, tiny_distance ), coordinates::from_degrees( 0.0, 0.0 ), tiny_epsilon ) );
+    }
+    // for reference, enforce NOT taking the 3rd branch of is_far that relies on unstable computations
+    {
+        double tiny_distance = rad2deg( 16.0 / 6371000.0 );   // 16 m at the Earth radius; still not zero
+        double tiny_epsilon = deg2rad(0.5 * tiny_distance);
+        EXPECT_FALSE( tiny_epsilon < coordinates::epsilon );
+        // epsilon is smaller then distance, cannot be near; but angle is zero and is reported as near
+        EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( tiny_distance, 0.0 ), coordinates::from_degrees( 0.0, 0.0 ), tiny_epsilon ) );
+        EXPECT_FALSE( coordinates::is_near( coordinates::from_degrees( 0.0, tiny_distance ), coordinates::from_degrees( 0.0, 0.0 ), tiny_epsilon ) );
     }
 }
