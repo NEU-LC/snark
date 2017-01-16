98db509cf8711350e5775a050cad49440c2747d4
Dmitry Mikhin <d.mikhin@acfr.usyd.edu.au>
2017-01-16 14:27:26 +1100

    Move the unit test, to let space for more tests.

--- imaging/test/cv-cat/ratio/parser/ratio-parser_test.cpp
+++ imaging/test/cv-cat/ratio/parser/ratio-parser_test.cpp
@@ -27,7 +27,7 @@
 // OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 // IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "../../../../imaging/cv_mat/ratio.h"
+#include "../../../../../imaging/cv_mat/ratio.h"
 
 #include <gtest/gtest.h>
 
