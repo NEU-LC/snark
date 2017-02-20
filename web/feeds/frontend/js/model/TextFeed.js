4b9b8db0a569147a608b4c1dd68657430c8d0915
v.satpute <v.satpute@acfr.usyd.edu.au>
2017-02-20 12:41:42 +1100

    Added form support for general widget, Snark - webframes : added code to handle error conditions and throw it as error 500, UI changes to handle errors.

--- web/feeds/frontend/js/model/TextFeed.js
+++ web/feeds/frontend/js/model/TextFeed.js
@@ -6,10 +6,12 @@ define('TextFeed', ["jquery", "Feed"], function ($) {
     var TextFeed = function (feed_name, feed_path, config) {
         this.base = Feed;
         this.base(feed_name, feed_path, config);
+        this.height = this.config.height != undefined ? this.config.height : 400;
     };
     TextFeed.prototype = Object.create(Feed.prototype);
     TextFeed.prototype.load = function () {
         $.ajax({
+            crossDomain: true,
             context: this,
             url: this.get_url(),
             timeout: globals.timeout
@@ -18,14 +20,30 @@ define('TextFeed', ["jquery", "Feed"], function ($) {
         }).fail(function (jqXHR, textStatus, errorThrown) {
             this.onerror();
         });
+        if (this.height > 0) {
+            this.target.height(this.height);
+        }
     };
     TextFeed.prototype.onload_ = function (data) {
         if (data && data.length && data[data.length - 1] == '\n') {
             data = data.substring(0, data.length - 1);
         }
+        var orig_data = data;
         data = data ? data : '&nbsp;';
-        this.target.append('<tr><td><pre>' + data + '</pre></td></tr>');
+        this.target.append('<tr><td><pre class="text-pre">' + data + '</pre></td></tr>');
+        var this_ = this;
+        var panel = $(this.target).find(".text-pre");
+        if (orig_data) {
+            $(panel).css("overflow-y", "scroll");
+        }
+        else {
+            $(panel).css("overflow", "auto");
+        }
+        $(panel).css("max-height", function () {
+            return this_.height;
+        });
         this.draw();
+        $(panel).scrollTop($(panel)[0].scrollHeight);
     };
     TextFeed.prototype.draw = function () {
         while (this.target.find('tbody tr').length > this.config.text.show_items) {
