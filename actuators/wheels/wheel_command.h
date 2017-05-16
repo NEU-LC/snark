098b2c39e9373907a3bfdb436211db6f09f6a11b
Aneel Rathore <a.rathore@acfr.usyd.edu.au>
2017-05-16 12:31:05 +1000

    minor comment fix

--- actuators/wheels/wheel_command.h
+++ actuators/wheels/wheel_command.h
@@ -55,7 +55,7 @@ struct wheel_command
 };
 
 // angle limits from a start angle (min) to the end angle (max) in counter clockwise direction, where min,max in [-pi,pi].
-// Hence, it is possible that min > max. For example a limit of [pi/4,-pi/4] implies [90,270] in degrees.
+// It is possible that min > max, for example a limit of [pi/2,-pi/2] implies [90,270] limit in degrees.
 struct limit
 {
     double min;
