9a919b6491f51e170c5eade682713a3003d14539
Dmitry Mikhin <d.mikhin@acfr.usyd.edu.au>
2017-05-25 16:11:26 +1000

    image-color-calc: implemented parsing the command line to get the requested conversion; multiple ways to specify the conversion.

--- imaging/applications/image-color-calc.cpp
+++ imaging/applications/image-color-calc.cpp
@@ -16,12 +16,11 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with Ark. If not, see <http://www.gnu.org/licenses/>.
 
-#include <opencv2/core/core.hpp>
 #include <comma/visiting/traits.h>
 #include <comma/csv/stream.h>
 #include <comma/string/string.h>
-#include <comma/math/interval.h>
-#include "../../imaging/cv_mat/serialization.h"
+
+#include <boost/algorithm/string/predicate.hpp>
 
 #include <type_traits>
 
@@ -87,6 +86,57 @@ namespace {
         exit( 0 );
     }
 
+    // enums do not have ctors, sigh
+    struct colorspace
+    {
+        enum cspace {
+            none = 0,
+            rgb,
+            ycrcb
+        };
+
+        cspace value;
+        colorspace( const std::string & s ) : value( boost::iequals( s, "rgb" ) ? rgb : ( boost::iequals( s, "ycrcb" ) ? ycrcb : none ) ) { }
+        colorspace( colorspace::cspace v ) : value( v ) { }
+
+        operator std::string() const {
+            switch( value ) {
+                case( none ):  return "none";  break;
+                case( rgb ):   return "rgb";   break;
+                case( ycrcb ): return "ycrcb"; break;
+            }
+            return "none"; // avoid a warning
+        };
+
+        static std::vector< std::string > field_names( cspace c )
+        {
+            static std::map< cspace, std::vector< std::string > > m = {
+                { rgb, comma::split( "r,g,b" , ',' ) },
+                { ycrcb, comma::split( "y,cr,cb", ',' ) },
+                { none, comma::split( "channel0,channel1,channel2", ',' ) }
+            };
+            return m[ c ];
+        }
+
+        void set_to_opposite_if_not_given( const colorspace & other )
+        {
+            if ( value != colorspace::none ) { return; }
+            // this simple logic would break if we have more then 2 colorspaces
+            switch ( other.value ) {
+                case colorspace::rgb:    value = colorspace::ycrcb; break;
+                case colorspace::ycrcb:  value = colorspace::rgb;   break;
+                case colorspace::none:
+                    COMMA_THROW( comma::exception, "cannot set the colorspace value to the opposite of " << std::string( other ) );
+            }
+        }
+    };
+
+    std::ostream & operator<<( std::ostream & os, const colorspace & c )
+    {
+        os << std::string( c );
+        return os;
+    }
+
     // do not call fields RGB because can contain YCrCb or any other color space
     template< typename T >
     struct pixel
@@ -136,6 +186,49 @@ namespace {
     };
 
     // to continue, ub-to-floating-point, longer integers (short, int)?
+
+    bool fields_have_required( const std::vector< std::string > & fields, const std::vector< std::string > & required )
+    {
+        for ( const auto & r : required ) {
+            if ( std::find( fields.begin(), fields.end(), r ) == fields.end() ) { return false; }
+        }
+        return true;
+    }
+
+    colorspace get_colorspace_from_fields( const std::vector< std::string > & fields, const std::vector< colorspace > & spaces )
+    {
+        if ( spaces.empty() ) { COMMA_THROW( comma::exception, "no colorspaces provided to choose from" ); }
+        unsigned int total = 0;
+        colorspace rv( colorspace::none );
+        for ( const auto & c : spaces ) {
+            if ( fields_have_required( fields, colorspace::field_names( c.value ) ) ) {
+                ++total;
+                rv = c;
+            }
+        }
+        if ( total > 1 ) { COMMA_THROW( comma::exception, "contradictory field names match multiple colorspaces" ); }
+        if ( total == 0 ) { COMMA_THROW( comma::exception, "field names do not match the requested colorspaces "
+                                                           << std::accumulate( std::next( spaces.begin() ), spaces.end(), std::string( spaces.front() )
+                                                                             , []( const colorspace & c, const std::string & a ) { return a + "," + std::string( c ); } ) ); }
+        return rv;
+    }
+
+    void rename_fields_to_channels( std::vector< std::string > & fields, const colorspace & c )
+    {
+        const std::vector< std::string > & channels = colorspace::field_names( colorspace::none );
+        const std::vector< std::string > & own = colorspace::field_names( c.value );
+        std::map< std::string, std::string > map;
+        std::transform( own.begin(), own.end(), channels.begin(), std::inserter( map, map.end() ), []( const std::string & k, const std::string & v ) { return std::make_pair( k, v ); } );
+        for ( size_t i = 0; i < fields.size(); ++i ) { auto search = map.find( fields[i] ); if ( search != map.end() ) { fields[i] = search->second; } }
+    }
+
+    void setup_fields_for_colorspace( std::vector< std::string > & fields, const colorspace & c )
+    {
+        std::vector< colorspace > spaces( 1, c );
+        if ( c.value != colorspace::none ) { spaces.push_back( colorspace::none ); }
+        colorspace found = get_colorspace_from_fields( fields, spaces );
+        if ( found.value != colorspace::none ) { rename_fields_to_channels( fields, c ); }
+    }
 
 } // anonymous
 
@@ -187,12 +280,56 @@ int main( int ac, char** av )
         comma::command_line_options options( ac, av, usage );
         comma::csv::options csv( options );
         csv.full_xpath = true;
-        using snark::cv_mat::serialization;
         verbose = options.exists("--verbose,-v");
-        std::vector< std::string > ops = options.unnamed("-h,--help,-v,--verbose,--flush,--input-fields,--input-format,--output-fields,--output-format,--show-partial,--discard", "--fields,--binary,--input");
+        std::vector< std::string > ops = options.unnamed("-h,--help,-v,--verbose,--flush,--input-fields", "--fields,-f,--binary,-b,--format,--to,--from");
         if( ops.empty() ) { std::cerr << name << "please specify an operation." << std::endl; return 1;  }
         if( ops.size() > 1 ) { std::cerr << name << "please specify only one operation, got " << comma::join( ops, ' ' ) << std::endl; return 1; }
         std::string operation = ops.front();
+        std::vector< std::string > fields = comma::split( csv.fields, csv.delimiter );
+
+        // the user may specify the direction of conversion by many ways
+        // if --from is specified:
+        //     if fields are not given, fields are set to the from-specific defaults
+        //     if fields are given, the required fields must be present (and renamed if needed)
+        // otherwise, if --fields is given, infer the from colorspace from fields
+        // otherwise, if --to is given, infer the fields and from colorspace from --to
+        // finally, if none of the above, apply defaults
+        colorspace toc( options.value< std::string >( "--to", "none" ) );
+        colorspace fromc( options.value< std::string >( "--from", "none" ) );
+        if ( fromc.value != colorspace::none ) {
+            if ( options.exists( "--fields,-f" ) )
+            {
+                setup_fields_for_colorspace( fields, fromc );
+                csv.fields = comma::join( fields, ',' );
+            } else {
+                csv.fields = "channel0,channel1,channel2";
+            }
+            toc.set_to_opposite_if_not_given( fromc );
+        } else {
+            if ( options.exists( "--fields,-f" ) )
+            {
+                std::vector< colorspace > spaces = { colorspace( colorspace::rgb ), colorspace( colorspace::ycrcb ) };
+                fromc = get_colorspace_from_fields( fields, spaces );
+                // now fromc cannot be none
+                rename_fields_to_channels( fields, fromc );
+                csv.fields = comma::join( fields, ',' );
+                toc.set_to_opposite_if_not_given( fromc );
+            } else {
+                // '--from' not given, '--fields' not given, infer from '--to'
+                // this simple logic would break if there are more than 2 colorspaces
+                switch ( toc.value ) {
+                    case colorspace::rgb:   fromc.value = colorspace::ycrcb; break;
+                    case colorspace::ycrcb: fromc.value = colorspace::rgb;   break;
+                    case colorspace::none:
+                        // last resort, all defaults
+                        fromc.value = colorspace::rgb;
+                        toc.value = colorspace::ycrcb;
+                        break;
+                }
+                csv.fields = "channel0,channel1,channel2";
+            }
+        }
+        std::cerr << "convert from " << fromc << " to " << toc << " using input fields " << csv.fields << std::endl;
 #if 0
         if( operation == "mean" )
         {
