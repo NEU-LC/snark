ff93ae89b904fc752f91c4cd7c3a4da07fab3e10
Dmitry Mikhin <d.mikhin@acfr.usyd.edu.au>
2017-06-14 22:54:38 +1000

    image-color-calc: got rid of auxiliary POD data type and conversions to and from it. Use std::vector instead.

--- imaging/color/convert.cpp
+++ imaging/color/convert.cpp
@@ -37,46 +37,40 @@ namespace {
 
     using namespace snark::imaging;
 
-    struct pod
-    {
-        pod( double c0, double c1, double c2 ) : channel0( c0 ), channel1( c1 ), channel2( c2 ) { }
-        double channel0;
-        double channel1;
-        double channel2;
-    };
+    typedef std::vector< double > channels;
 
     // conversion does not have to be a linear operation, but many are; these function simplify setting up such conversions
-    pod linear_combination( const pod & i, const Eigen::Vector3d & before, const Eigen::Matrix3d & m, const Eigen::Vector3d & after )
+    channels linear_combination( const channels & i, const Eigen::Vector3d & before, const Eigen::Matrix3d & m, const Eigen::Vector3d & after )
     {
-        pod t( i.channel0 + before(0), i.channel1 + before(1), i.channel2 + before(2) );
-        return pod( m(0,0) * t.channel0 + m(0,1) * t.channel1 + m(0,2) * t.channel2 + after(0)
-                  , m(1,0) * t.channel0 + m(1,1) * t.channel1 + m(1,2) * t.channel2 + after(1)
-                  , m(2,0) * t.channel0 + m(2,1) * t.channel1 + m(2,2) * t.channel2 + after(2) );
+        channels t = { i[0] + before(0), i[1] + before(1), i[2] + before(2) };
+        return channels( { m(0,0) * t[0] + m(0,1) * t[1] + m(0,2) * t[2] + after(0)
+                         , m(1,0) * t[0] + m(1,1) * t[1] + m(1,2) * t[2] + after(1)
+                         , m(2,0) * t[0] + m(2,1) * t[1] + m(2,2) * t[2] + after(2) } );
     }
 
-    pod linear_combination( const pod & i, const Eigen::Vector3d & before, const Eigen::Matrix3d & m )
+    channels linear_combination( const channels & i, const Eigen::Vector3d & before, const Eigen::Matrix3d & m )
     {
-        pod t( i.channel0 + before(0), i.channel1 + before(1), i.channel2 + before(2) );
-        return pod( m(0,0) * t.channel0 + m(0,1) * t.channel1 + m(0,2) * t.channel2
-                  , m(1,0) * t.channel0 + m(1,1) * t.channel1 + m(1,2) * t.channel2
-                  , m(2,0) * t.channel0 + m(2,1) * t.channel1 + m(2,2) * t.channel2 );
+        channels t = { i[0] + before(0), i[1] + before(1), i[2] + before(2) };
+        return channels( { m(0,0) * t[0] + m(0,1) * t[1] + m(0,2) * t[2]
+                         , m(1,0) * t[0] + m(1,1) * t[1] + m(1,2) * t[2]
+                         , m(2,0) * t[0] + m(2,1) * t[1] + m(2,2) * t[2] } );
     }
 
-    pod linear_combination( const pod & i, const Eigen::Matrix3d & m, const Eigen::Vector3d & after )
+    channels linear_combination( const channels & i, const Eigen::Matrix3d & m, const Eigen::Vector3d & after )
     {
-        return pod( m(0,0) * i.channel0 + m(0,1) * i.channel1 + m(0,2) * i.channel2 + after(0)
-                  , m(1,0) * i.channel0 + m(1,1) * i.channel1 + m(1,2) * i.channel2 + after(1)
-                  , m(2,0) * i.channel0 + m(2,1) * i.channel1 + m(2,2) * i.channel2 + after(2) );
+        return channels( { m(0,0) * i[0] + m(0,1) * i[1] + m(0,2) * i[2] + after(0)
+                         , m(1,0) * i[0] + m(1,1) * i[1] + m(1,2) * i[2] + after(1)
+                         , m(2,0) * i[0] + m(2,1) * i[1] + m(2,2) * i[2] + after(2) } );
     }
 
-    pod linear_combination( const pod & i, const Eigen::Matrix3d & m )
+    channels linear_combination( const channels & i, const Eigen::Matrix3d & m )
     {
-        return pod( m(0,0) * i.channel0 + m(0,1) * i.channel1 + m(0,2) * i.channel2
-                  , m(1,0) * i.channel0 + m(1,1) * i.channel1 + m(1,2) * i.channel2
-                  , m(2,0) * i.channel0 + m(2,1) * i.channel1 + m(2,2) * i.channel2 );
+        return channels( { m(0,0) * i[0] + m(0,1) * i[1] + m(0,2) * i[2]
+                         , m(1,0) * i[0] + m(1,1) * i[1] + m(1,2) * i[2]
+                         , m(2,0) * i[0] + m(2,1) * i[1] + m(2,2) * i[2] } );
     }
 
-    typedef std::function< pod ( const pod & p ) > C;
+    typedef std::function< channels ( const channels & p ) > C;
     // TODO:
     // - store pod as std::vector instead of introducing a POD type and copying
     // - YPbPr is from -0.5 to +0.5, not 0 to 1; introduce a new range to match
@@ -89,23 +83,23 @@ namespace {
     {
         static conversion_map_t m;
         m[ std::make_pair( std::make_pair( colorspace::rgb, ub ), std::make_pair( colorspace::rgb,   ub ) ) ] =
-            []( const pod & i ){ return linear_combination( i, (Eigen::Matrix3d() << 1, 0, 0, 0, 1, 0, 0, 0, 1).finished() ); };
+            []( const channels & i ){ return linear_combination( i, (Eigen::Matrix3d() << 1, 0, 0, 0, 1, 0, 0, 0, 1).finished() ); };
         m[ std::make_pair( std::make_pair( colorspace::rgb, f  ), std::make_pair( colorspace::ypbpr, f  ) ) ] =
-            []( const pod & i ){ return linear_combination( i, (Eigen::Matrix3d() << 0.299, 0.587, 0.114, -0.168736, -0.331264, 0.5, 0.5, -0.418688, -0.081312).finished(), (Eigen::Vector3d() << 0, 0.5, 0.5).finished() ); };
+            []( const channels & i ){ return linear_combination( i, (Eigen::Matrix3d() << 0.299, 0.587, 0.114, -0.168736, -0.331264, 0.5, 0.5, -0.418688, -0.081312).finished(), (Eigen::Vector3d() << 0, 0.5, 0.5).finished() ); };
         m[ std::make_pair( std::make_pair( colorspace::rgb, d  ), std::make_pair( colorspace::ypbpr, d  ) ) ] = m[ std::make_pair( std::make_pair( colorspace::rgb, f  ), std::make_pair( colorspace::ypbpr, f  ) ) ];
         m[ std::make_pair( std::make_pair( colorspace::rgb, f  ), std::make_pair( colorspace::ypbpr, d  ) ) ] = m[ std::make_pair( std::make_pair( colorspace::rgb, f  ), std::make_pair( colorspace::ypbpr, f  ) ) ];
         m[ std::make_pair( std::make_pair( colorspace::rgb, d  ), std::make_pair( colorspace::ypbpr, f  ) ) ] = m[ std::make_pair( std::make_pair( colorspace::rgb, f  ), std::make_pair( colorspace::ypbpr, f  ) ) ];
         m[ std::make_pair( std::make_pair( colorspace::rgb, f  ), std::make_pair( colorspace::ycbcr, ub ) ) ] =
-            []( const pod & i ){ return linear_combination( i, (Eigen::Matrix3d() << 65.481, 128.553, 24.966, -37.797, -74.203, 112.0, 112.0, -93.786, -18.214).finished(), (Eigen::Vector3d() << 16, 128, 128).finished() ); };
+            []( const channels & i ){ return linear_combination( i, (Eigen::Matrix3d() << 65.481, 128.553, 24.966, -37.797, -74.203, 112.0, 112.0, -93.786, -18.214).finished(), (Eigen::Vector3d() << 16, 128, 128).finished() ); };
         m[ std::make_pair( std::make_pair( colorspace::rgb, d  ), std::make_pair( colorspace::ycbcr, ub ) ) ] = m[ std::make_pair( std::make_pair( colorspace::rgb, f  ), std::make_pair( colorspace::ycbcr, ub ) ) ];
         m[ std::make_pair( std::make_pair( colorspace::rgb, ub ), std::make_pair( colorspace::ycbcr, ub ) ) ] =
-            []( const pod & i ){ return linear_combination( i, (Eigen::Matrix3d() << 65.738, 129.057, 25.064, -37.945, -74.494, 112.439, 112.439, -94.154, -18.258).finished() / 256., (Eigen::Vector3d() << 16, 128, 128).finished() ); };
+            []( const channels & i ){ return linear_combination( i, (Eigen::Matrix3d() << 65.738, 129.057, 25.064, -37.945, -74.494, 112.439, 112.439, -94.154, -18.258).finished() / 256., (Eigen::Vector3d() << 16, 128, 128).finished() ); };
         {
             m[ std::make_pair( std::make_pair( colorspace::ycbcr, ub ), std::make_pair( colorspace::rgb, ub ) ) ] =
-                []( const pod & i) {
-                    return pod( 255/219. * ( i.channel0 - 16 )                                                     + 255/112.*0.701             * ( i.channel2 - 128 )
-                              , 255/219. * ( i.channel0 - 16 ) - 255/112.*0.886*0.114/0.587 * ( i.channel1 - 128 ) - 255/112.*0.701*0.299/0.587 * ( i.channel2 - 128 )
-                              , 255/219. * ( i.channel0 - 16 ) + 255/112.*0.886             * ( i.channel1 - 128 )                                                     );
+                []( const channels & i) {
+                    return channels( { 255/219. * ( i[0] - 16 )                                               + 255/112.*0.701             * ( i[2] - 128 )
+                                     , 255/219. * ( i[0] - 16 ) - 255/112.*0.886*0.114/0.587 * ( i[1] - 128 ) - 255/112.*0.701*0.299/0.587 * ( i[2] - 128 )
+                                     , 255/219. * ( i[0] - 16 ) + 255/112.*0.886             * ( i[1] - 128 )                                               } );
                 };
         }
         return m;
@@ -143,9 +137,8 @@ namespace {
         {
             const pixel< double, inr > * p = is.read();
             if( !p ) { break; }
-            pod i( p->channel[0], p->channel[1], p->channel[2] );
-            pod o( c( i ) );
-            pixel< double, outr > op( o.channel0, o.channel1, o.channel2 );
+            pixel< double, outr > op;
+            c( p->channel ).swap( op.channel );
             tied.append( pixel< outt, outr >::convert( op ) );
             if ( output_csv.flush ) { std::cout.flush(); }
         }
