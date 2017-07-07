780f7e4f2e551783b51e1e808d60077c7cd153ad
dewey <dewey@acfr.usyd.edu.au>
2017-07-07 16:36:33 +1000

    detail/accumulated.h: implementing sliding_window_impl, average sliding window is done

--- imaging/cv_mat/detail/accumulated.cpp
+++ imaging/cv_mat/detail/accumulated.cpp
@@ -93,14 +93,35 @@ typename accumulated_impl_< H >::value_type accumulated_impl_< H >::operator()( 
     return value_type( n.first, result );
 }
 
+template < int DepthIn >
 static double sliding_average( double in, double avg, comma::uint64 count, 
                                comma::uint32 row, comma::uint32 col, 
-                               const std::deque< cv::Mat >& window)
+                               const std::deque< cv::Mat >& window, comma::uint32 size)
 {
-    // Let do the simple one first
-    return 1.0;
+    typedef typename depth_traits< DepthIn >::value_t value_in_t;
+    // Haven't reach the window size yet
+    if( window.size() < size ) { return (avg + (in - avg)/count); }
+    else
+    {
+        const auto* back_of_window = window.front().ptr< value_in_t >(row);
+        value_in_t val = *(back_of_window + col);
+        return avg + (in - val)/double(size);
+    }
 }
 
+static apply_function get_average(int depth, const std::deque< cv::Mat >& window, comma::uint32 size)
+{
+    switch( depth )
+    {
+        case CV_8U : return boost::bind( &sliding_average< CV_8U >, _1, _2, _3, _4, _5, boost::ref(window), size );
+        case CV_8S : return boost::bind( &sliding_average< CV_8S >, _1, _2, _3, _4, _5, boost::ref(window), size );
+        case CV_16U: return boost::bind( &sliding_average< CV_16U >, _1, _2, _3, _4, _5, boost::ref(window), size );
+        case CV_16S: return boost::bind( &sliding_average< CV_16S >, _1, _2, _3, _4, _5, boost::ref(window), size );
+        case CV_32S: return boost::bind( &sliding_average< CV_32S >, _1, _2, _3, _4, _5, boost::ref(window), size );
+        case CV_32F: return boost::bind( &sliding_average< CV_32F >, _1, _2, _3, _4, _5, boost::ref(window), size );
+        case CV_64F: return boost::bind( &sliding_average< CV_64F >, _1, _2, _3, _4, _5, boost::ref(window), size );
+    }
+}
     
 template < typename H >
 typename sliding_window_impl_< H >::value_type sliding_window_impl_< H >::operator()( const typename sliding_window_impl_< H >::value_type& n )
@@ -118,7 +139,7 @@ typename sliding_window_impl_< H >::value_type sliding_window_impl_< H >::operat
         }
     }
     
-    apply_function average = boost::bind( &sliding_average, _1, _2, _3, _4, _5, boost::ref(window_) );
+    apply_function average = get_average(n.second.depth(), window_, size_);
     switch (type_)
     {
         case accumulated_type::average: iterate_by_input_type< H >(n.second, result_, average , count_); break;
@@ -126,6 +147,8 @@ typename sliding_window_impl_< H >::value_type sliding_window_impl_< H >::operat
         case accumulated_type::min: iterate_by_input_type< H >(n.second, result_, &accumulated_min, count_); break;
     }
     
+    if( window_.size() == size_ ) { window_.pop_front(); }
+    window_.push_back( n.second );
     
     cv::Mat result;
     result_.convertTo(result, n.second.type());
