73d38514abf7b136b6dd6b2b61b98de751f32f13
dewey <dewey@acfr.usyd.edu.au>
2017-07-31 14:35:45 +1000

    Revert "hokuyo-to-csv: re-arranged usage to be alphabetical, minor refactor"
    This reverts commit 1315e6a88a0bd16c197f53b9cd5e4baec5c6dd7f.

--- sensors/lidars/hokuyo/applications/hokuyo-to-csv.cpp
+++ sensors/lidars/hokuyo/applications/hokuyo-to-csv.cpp
@@ -56,10 +56,11 @@ static void usage()
     std::cerr << "By default it scans using 1081 steps/rays/data points as fast as possible, you can limit it to 271 steps with --start-step." << std::endl;
     std::cerr << std::endl;
     std::cerr << "usage" << std::endl;
-    std::cerr << "    hokuyo-to-csv --laser <host:port> [--serial] [ --fields t,x,y,z,range,bearing,elevation,intensity ]" << std::endl;
-    std::cerr << "      also supports serial device that uses scip2 data communication, see --serial" << std::endl;
+    std::cerr << "    hokuyo-to-csv --laser <host:port> [ --fields t,x,y,z,range,bearing,elevation,intensity ]" << std::endl;
     std::cerr << std::endl;
     std::cerr << "options" << std::endl;
+    std::cerr << "    --laser=:             the TCP connection to the laser <host:port>" << std::endl;
+    std::cerr << "    --help,-h:            show this message" << std::endl;
     std::cerr << "    --binary,-b:          output binary equivalent of csv" << std::endl;
     std::cerr << "    --fields=<fields>:    output only given fields" << std::endl;
 //     std::cerr << "        default: " << comma::join( comma::csv::names< csv_point >( false ), ',' ) << " (" << comma::csv::format::value< csv_point >() << ")" << std::endl;
@@ -67,18 +68,13 @@ static void usage()
     std::cerr << "        x,y,z:            cartesian coordinates in sensor frame, where <0,0,0> is no data" << std::endl;
     std::cerr << "        range,bearing,elevation or r,b,e: polar coordinates in sensor frame" << std::endl;
     std::cerr << "        intensity or i:   intensity of the data point" << std::endl;
-    std::cerr << "    --laser=:             the TCP connection to the laser <host:port>" << std::endl;
-    std::cerr << "    --help,-h:            show this message" << std::endl;
-    std::cerr << "    --output-fields:      output fields to stdout and exit; requires --serial or --non-serial" << std::endl;
-    std::cerr << "    --output-format,--format: output binary format for given fields to stdout and exit; requires --serial and --non-serial" << std::endl;
-    std::cerr << "    --reboot-on-error:    if failed to put scanner into scanning mode, reboot the scanner." << std::endl;
-    std::cerr << "    --non-serial|--non-scip2:" << std::endl;
-    std::cerr << "                          opposite of --serial, default mode if omitted." << std::endl;
-    std::cerr << "    --num-of-scans:       How many scans is requested for ME requests, default is 100 - 0 for continuous ( data verification problem with 0 )." << std::endl;
-    std::cerr << "    --scan-break:         How many usec of sleep time between ME request and reponses received before issuing another ME request, default is 20us." << std::endl;
-    std::cerr << "    --serial|--scip2:     the target device type uses scip2 data exchange." << std::endl;
+    std::cerr << "    --output-fields:      output fields to stdout and exit; use --scip2 for scip2 fields" << std::endl;
+    std::cerr << "    --output-format,--format: output binary format for given fields to stdout and exit" << std::endl;
     std::cerr << "    --start-step=<0-890>: Scan starting at a start step and go to (step+270) wich covers 67.75\" which is 270\"/4." << std::endl;
     std::cerr << "                          Does not perform a full 270\" scan." << std::endl;
+    std::cerr << "    --reboot-on-error:    if failed to put scanner into scanning mode, reboot the scanner." << std::endl;
+    std::cerr << "    --num-of-scans:       How many scans is requested for ME requests, default is 100 - 0 for continuous ( data verification problem with 0 )." << std::endl;
+    std::cerr << "    --scan-break:         How many usec of sleep time between ME request and reponses received before issuing another ME request, default is 20us." << std::endl;
     std::cerr << "    --verbose: show more information" << std::endl;
     std::cerr << std::endl;
     std::cerr << "Output" << std::endl;
@@ -177,9 +173,6 @@ void output_samples()
 static const int UST_MAX_STEPS = 1081;
 static const int UST_SMALL_STEPS = 271;
 
-// todo
-// - --help: options in alphabetic order
-// - --non-serial: i am not sure what to do with it; what is non-serial hokuyo?
 
 int main( int ac, char** av )
 {
@@ -198,20 +191,18 @@ int main( int ac, char** av )
         frames=options.value<int>("--frames",-1);
         omit_on_error = ! options.exists("--dont-omit-on-error");
         bool permissive = options.exists( "--permissive" );
-        std::vector< std::string > unnamed = options.unnamed( "--output-fields,--output-format,--format,--verbose,-v,--flush,--reboot-on-error,--debug,--scip2,--non-scip2,--output-samples,--dont-omit-on-errork,--permissive",
-                                                              "--frames,--scan-break,--num-of-scans,--start-step,--end-step,--serial,--non-serial,--port,--laser,--fields,--binary,-b,--baud-rate,--set-baud-rate");
+        std::vector< std::string > unnamed = options.unnamed( "--output-fields,--output-format,--format,--verbose,-v,--flush,--reboot-on-error,--debug,--scip2,--output-samples,--dont-omit-on-errork,--permissive",
+                                                              "--frames,--scan-break,--num-of-scans,--start-step,--end-step,--serial,--port,--laser,--fields,--binary,-b,--baud-rate,--set-baud-rate");
         if(!unnamed.empty())
         {
             std::cerr<<"invalid option(s):"<< comma::join(unnamed, ',') <<std::endl;
             return 1;
         }
         
-        if( options.exists("--scip2,--serial") &&  options.exists("--non-scip2,--non-serial") ) { COMMA_THROW(comma::exception, "please specify only --serial or --non-serial, not both"); }
-        bool serial=options.exists("--scip2,--serial");
-        
         scip2_device scip2;
         ust_device<UST_SMALL_STEPS> ust_small( permissive );
         ust_device<UST_MAX_STEPS> ust_max( permissive );
+        bool serial=options.exists("--scip2") || options.exists( "--serial");
         laser_device& device = serial ? scip2 : (start_step ?  (laser_device&)ust_small : (laser_device&)ust_max);
         
         // Sets up output data
@@ -229,8 +220,6 @@ int main( int ac, char** av )
         // see sick-ldmrs-to-csv
         if( options.exists( "--output-format,--format" ) ) 
         {
-            if( !options.exists("--scip2,--serial") &&  !options.exists("--non-scip2,--non-serial") ) { std::cerr << "hokuyo-to-csv: please specify --serial or --non-serial device" << std::endl; return 1; }
-            if( options.exists("--scip2,--serial") &&  options.exists("--non-scip2,--non-serial") ) { std::cerr << "hokuyo-to-csv: please specify only --serial or --non-serial, not both" << std::endl; return 1; }
             if(serial) { std::cout << comma::csv::format::value< scip2_device::output_t >( csv.fields, false ) << std::endl; }
             else { std::cout << comma::csv::format::value< snark::hokuyo::data_point >( csv.fields, false ) << std::endl; }
             return 0; 
@@ -238,8 +227,6 @@ int main( int ac, char** av )
         }
         if(options.exists("--output-fields"))
         {
-            if( !options.exists("--scip2,--serial") &&  !options.exists("--non-scip2,--non-serial") ) { std::cerr << "hokuyo-to-csv: please specify --serial or --non-serial device" << std::endl; return 1; }
-            if( options.exists("--scip2,--serial") &&  options.exists("--non-scip2,--non-serial") ) { std::cerr << "hokuyo-to-csv: please specify only --serial or --non-serial, not both" << std::endl; return 1; }
             if(!csv.fields.empty()) {std::cout<<csv.fields<<std::endl;}
             else if(serial) { std::cout<<comma::join(comma::csv::names<scip2_device::output_t>(), ',')<<std::endl;}
             else {std::cout<<comma::join(comma::csv::names<snark::hokuyo::data_point>(), ',')<<std::endl;}
@@ -259,17 +246,27 @@ int main( int ac, char** av )
         device.setup(*ios);
         if(serial)
         {
-            if(debug_verbose) { ((serial_stream&)*ios).dump_setting(); }
+            if(debug_verbose)
+                ((serial_stream&)*ios).dump_setting();
 //             debug_msg("change serial settings");
 //             const char* ss_cmd="SS500000\n";
             process(scip2);
         }
-        else {
-            if(start_step) { process(ust_small); } else { process(ust_max); }
+        else
+        {
+            if(start_step)
+                process(ust_small);
+            else
+                process(ust_max);
         }
-        return 0;
     }
-    catch( std::exception& ex ) { std::cerr << name() << ex.what() << std::endl; }
-    catch( ... ) { std::cerr << name() << "unknown exception" << std::endl; }
-    return 1;
+    catch( std::exception& ex )
+    {
+        std::cerr << name() << ex.what() << std::endl; return 1;
+    }
+    catch( ... )
+    {
+        std::cerr << name() << "unknown exception" << std::endl; return 1;
+    }
+    return 0;
 }
