1a4bdcc3e7ee458a59eb59b930d2836c633485f3
dewey <dewey@acfr.usyd.edu.au>
2017-08-18 15:13:14 +1000

    points-grep: reverting previous change as Jenkins does not have boost 1.58

--- point_cloud/applications/points-grep.cpp
+++ point_cloud/applications/points-grep.cpp
@@ -488,7 +488,9 @@ struct polygon_t
     }
     
     bool within( const point_t& g ) const  { return boost::geometry::within( g, polygon ); } // totally inside
-    bool outside( const point_t& g ) const { return !within(g) && !boost::geometry::intersects( g, boundary ); } // totally outside
+    bool outside( const point_t& g ) const { return !within(g); } // totally outside
+    // Again this needs Boost 1.58
+//     bool outside( const point_t& g ) const { return !boost::geometry::intersects( g, polygon ); } // totally outside
     bool within( const line_t& g ) const   { return boost::geometry::within( g[0], polygon ) && boost::geometry::within( g[1], polygon ) && !boost::geometry::intersects( g, boundary ); }
     bool outside( const line_t& g ) const  { return !boost::geometry::within( g[0], polygon ) && !boost::geometry::within( g[1], polygon ) && !boost::geometry::intersects( g, boundary ); }
     // Enable when Boost documentation is corrected, not compiling on Boost 1.58
