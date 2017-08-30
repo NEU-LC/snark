6d2d39d1c45c0ed1a45d1765a54cbdb0b78b5ec0
Navid P <navidp@acfr.usyd.edu.au>
2017-08-30 15:37:36 +1000

    python snark image: write added arguement file=sys.sydout

--- python/snark/imaging/cv_image.py
+++ python/snark/imaging/cv_image.py
@@ -71,10 +71,10 @@ def iterator(file):
         header = np.fromfile(file, image.header_dtype, 1)
         data = np.fromfile(file, data_dtype, 1)
 
-def write( image, flush=False ):
-    image.header.tofile( sys.stdout )
-    image.data.tofile( sys.stdout )
-    if flush: sys.stdout.flush()
+def write( image, flush=False, out_file=sys.stdout ):
+    image.header.tofile( out_file )
+    image.data.tofile( out_file )
+    if flush: out_file.flush()
 
 if __name__ == '__main__':
     for i in iterator(sys.stdin):
