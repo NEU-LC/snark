cb2e05e505c5872e5b1b387079272699540cf8c4
seva <seva@acfr.usyd.edu.au>
2017-11-13 14:52:15 +1100

    snark.ros.convert.from_csv-supported_types(): trivial refactoring

--- python/snark/ros/convert.py
+++ python/snark/ros/convert.py
@@ -35,8 +35,7 @@ def ros_message_to_csv_record( message, lengths={} ):
 def from_csv_supported_types( v ):
     if type( v ) != numpy.datetime64: return v
     microseconds = numpy.int64( v )
-    seconds = microseconds / 1000000
-    return rospy.Time( microseconds / 1000000, ( microseconds - seconds * 1000000 ) * 1000 )
+    return rospy.Time( microseconds / 1000000, ( microseconds % 1000000 ) * 1000 )
 
 def _ros_message_to_csv_record( message, lengths={}, prefix='' ):
     """
