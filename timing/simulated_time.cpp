42d83cb483faa3e8c5da8006382b4b9c82eaba94
Matthew Imhoff <m.imhoff@acfr.usyd.edu.au>
2018-01-25 15:14:12 +1100

    add simulated_time library

---
+++ timing/simulated_time.cpp
@@ -0,0 +1,55 @@
+// This file is part of snark, a generic and flexible library for robotics research
+// Copyright (c) 2011 The University of Sydney
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the University of Sydney nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "./simulated_time.h"
+
+// library for simulating system time with some shift
+
+namespace snark { namespace timing {
+
+simulated_time::simulated_time(): simulated(boost::posix_time::not_a_date_time) {}
+
+simulated_time::simulated_time(const boost::posix_time::ptime& t): simulated(t), start(boost::posix_time::microsec_clock::universal_time()) {}
+
+boost::posix_time::ptime simulated_time::operator()() const
+{
+    boost::posix_time::ptime t = boost::posix_time::microsec_clock::universal_time();
+    return is_set() ? t : ( simulated + ( t - start ) );
+}
+
+void simulated_time::reset(const boost::posix_time::ptime& t = boost::posix_time::not_a_date_time )
+{
+    simulated = t; 
+    start = boost::posix_time::microsec_clock::universal_time();
+}
+
+bool simulated_time::is_set() const { return !simulated.is_not_a_date_time(); }
+
+} } // namespace snark{ namespace timing
+
