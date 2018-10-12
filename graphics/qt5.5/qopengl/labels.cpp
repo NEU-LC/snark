d29251d2c336f42432ffce275677c32d0b13b211
Navid P <navidp@acfr.usyd.edu.au>
2018-10-12 12:38:16 +1100

    view-points added --font-size for labels

--- graphics/qt5.5/qopengl/labels.cpp
+++ graphics/qt5.5/qopengl/labels.cpp
@@ -36,6 +36,7 @@
 
 namespace snark { namespace graphics { namespace qopengl {
     
+int text_label::font_size=16;
 text_label::text_label(Eigen::Vector3d position, std::string text,color_t color) : position(position), text(text), color(color), width(1), height(1)
 {
 }
@@ -50,8 +51,8 @@ void text_label::update()
         fbo->bind();
         QOpenGLPaintDevice paint_dev(width, height);
         QPainter painter(&paint_dev);
-        painter.setFont(QFont("System",16));
-        QRect rect=painter.boundingRect(QRect(2,4,1000, 1000), Qt::AlignTop, QString(text.data()));
+        painter.setFont(QFont("System",font_size));
+        QRect rect=painter.boundingRect(QRect(0,0,1000, 1000), Qt::AlignTop, QString(text.data()));
         width=rect.width();
         height=rect.height();
         fbo->release();
@@ -65,11 +66,11 @@ void text_label::update()
 
 void text_label::draw(QPainter& painter)
 {
-    painter.setFont(QFont("System",16));
+    painter.setFont(QFont("System",font_size));
 //     painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
 //     painter.fillRect(0,0,width,height,Qt::green);
     painter.setPen( QColor(color.rgba[0]*255, color.rgba[1]*255, color.rgba[2]*255, color.rgba[3]*255 ) );
-    painter.drawText(QRect(2,4,width, height), Qt::AlignTop, QString(text.data()));
+    painter.drawText(QRect(0,0,width, height), Qt::AlignTop, QString(text.data()));
 }
 
 } } } // namespace snark { namespace graphics { namespace qopengl {
