5add67b2d246144614db5ce5e4db23b082e5bb2f
dave <dave@acfr.usyd.edu.au>
2018-11-05 18:10:48 +1100

    ocular-roboteye-thermal-cat: use boost::this_thread::sleep_for
    boost::thread::sleep is deprecated

--- sensors/roboteye/applications/ocular-roboteye-thermal-cat.cpp
+++ sensors/roboteye/applications/ocular-roboteye-thermal-cat.cpp
@@ -113,7 +113,7 @@ void capture_frame( ocular::RobotEyeThermal& roboteye_thermal
 {
     static unsigned int frame_num = 0;
     comma::verbose << "acquiring frame " << frame_num << std::endl;
-    boost::thread::sleep( boost::posix_time::microsec_clock::universal_time() + boost::posix_time::millisec( 1000 ));
+    boost::this_thread::sleep_for( boost::chrono::milliseconds( 1000 ));
     ::ocular::Image image;
     check_dev_status( roboteye_thermal.GetImage( image, image_mode ), "GetImage" );
 
@@ -240,7 +240,7 @@ int main( int argc, char** argv )
             if( capture && !acquiring )
             {
                 comma::verbose << "starting frame acquisition" << std::endl;
-                boost::thread::sleep( boost::posix_time::microsec_clock::universal_time() + boost::posix_time::millisec( 1000 ));
+                boost::this_thread::sleep_for( boost::chrono::milliseconds( 1000 ));
                 check_dev_status( roboteye_thermal.StartAcquisition(), "StartAcquisition" );
                 acquiring = true;
             }
