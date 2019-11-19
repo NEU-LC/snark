e4a54dc7df293bd171c1a7cc9bb6973c0bb14e0a
seva <seva>
2019-11-20 10:23:09 +1100

    imaging/cv_mat: contraharmonic: placeholder added

---
+++ imaging/cv_mat/detail/contraharmonic.cpp
@@ -0,0 +1,100 @@
+// This file is provided in addition to snark and is not an integral
+// part of snark library.
+// Copyright (c) 2019 Vsevolod Vlaskine
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// snark is a generic and flexible library for robotics research
+// Copyright (c) 2011 The University of Sydney
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the University of Sydney nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/// @author vsevolod vlaskine
+
+#pragma once
+
+#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/date_time/posix_time/ptime.hpp>
+#include <comma/base/exception.h>
+#include "contraharmonic.h"
+#include "utils.h"
+
+namespace snark { namespace cv_mat { namespace impl {
+
+template < typename H >
+contraharmonic< H >::contraharmonic( double power ): power_( power ) {} // todo
+
+template < typename H >
+std::pair< H, cv::Mat > contraharmonic< H >::operator()( std::pair< H, cv::Mat > m )
+{
+    COMMA_THROW( comma::exception, "contraharmonic: todo" );
+}
+
+template < typename H >
+std::pair< typename contraharmonic< H >::functor_t, bool > contraharmonic< H >::make( const std::string& options )
+{
+    COMMA_THROW( comma::exception, "contraharmonic: todo" );
+    return std::make_pair( contraharmonic< H >( 5 ), true ); // todo
+}
+
+template < typename H >
+typename std::string contraharmonic< H >::usage( unsigned int indent )
+{
+    std::string offset( indent, ' ' );
+    std::ostringstream oss;
+    oss << offset << "contraharmonic=<options>" << std::endl;
+    oss << offset << "    <options>" << std::endl;
+    oss << offset << "        todo" << std::endl;
+    return oss.str();
+}
+
+template class contraharmonic< boost::posix_time::ptime >;
+template class contraharmonic< std::vector< char > >;
+
+} } }  // namespace snark { namespace cv_mat { namespace impl {
