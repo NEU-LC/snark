0639af2e86a89580a8db578d7e19fd7efb18caec
Dave Jennings <dave.jennings@missionsystems.com.au>
2020-05-28 09:39:35 +1000

    ouster/packet.cpp: throw exception rather than print warning message
    1. that's better behaviour for a library
    2. don't be forgiving, there's no reason not to have the configuration and
       there'll be subtle errors in the data accuracy without it

--- sensors/lidars/ouster/packet.cpp
+++ sensors/lidars/ouster/packet.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2020 Mission Systems Pty Ltd
 
 #include "packet.h"
-#include <iostream>
+#include <comma/base/exception.h>
 
 namespace snark { namespace ouster { namespace OS1 {
 
@@ -10,20 +10,14 @@ beam_angle_lut_t get_beam_angle_lut( const beam_intrinsics_t& beam_intrinsics )
 {
     beam_angle_lut_t beam_angle_lut;
 
-    // not set - that's okay but not ideal, return lut with no corrections
-    if( beam_intrinsics.beam_altitude_angles.size() == 0 && beam_intrinsics.beam_azimuth_angles.size() == 0 ) { return beam_angle_lut; }
-
-    // incorrectly set - flag the error then return lut with no corrections
     if( beam_intrinsics.beam_altitude_angles.size() != beam_angle_lut.size() ||
         beam_intrinsics.beam_azimuth_angles.size() != beam_angle_lut.size() )
     {
-        std::cerr << "ouster: expected " << beam_angle_lut.size() << " intrinsic beam angles, got "
-                  << beam_intrinsics.beam_altitude_angles.size() << " altitude angles and "
-                  << beam_intrinsics.beam_azimuth_angles.size() << " azimuth angles" << std::endl;
-        return beam_angle_lut;
+        COMMA_THROW( comma::exception, "expected " << beam_angle_lut.size() << " intrinsic beam angles, got "
+                                       << beam_intrinsics.beam_altitude_angles.size() << " altitude angles and "
+                                       << beam_intrinsics.beam_azimuth_angles.size() << " azimuth angles" );
     }
 
-    // correctly set - set lut
     for( unsigned int i = 0; i < ouster::OS1::pixels_per_column; i++ )
     {
         beam_angle_lut[i] = beam_angle_lut_entry( beam_intrinsics.beam_altitude_angles[i] * 2 * M_PI / 360.0
