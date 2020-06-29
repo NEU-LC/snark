5815ea2c523b162518e071536021771c6616d06f
Dave Jennings <dave.jennings@missionsystems.com.au>
2020-06-29 16:09:19 +1000

    ouster-to-csv: remove unrequired setting of csv.full_xpath

--- sensors/lidars/ouster/applications/ouster-to-csv.cpp
+++ sensors/lidars/ouster/applications/ouster-to-csv.cpp
@@ -160,7 +160,6 @@ struct app
         char buf[ sizeof( I )];
 
         comma::csv::options output_csv;
-        output_csv.full_xpath = true;
         output_csv.format( comma::csv::format::value< O >() );
         comma::csv::binary_output_stream< O > os( std::cout, output_csv );
 
