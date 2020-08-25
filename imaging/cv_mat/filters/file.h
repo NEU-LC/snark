ed56fa9812af7e97ba19a92fdc2c0d5af69ba23f
vlaskine <vsevolod.vlaskine@gmail.com>
2020-08-25 18:50:20 +1000

    imaging/file: exit-if-done implemented, refactored

--- imaging/cv_mat/filters/file.h
+++ imaging/cv_mat/filters/file.h
@@ -1,31 +1,5 @@
-// This file is part of snark, a generic and flexible library for robotics research
 // Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2019 Vsevolod Vlaskine
 
 #pragma once
 
@@ -46,15 +20,22 @@ class file
 
         file( const get_timestamp_functor& get_timestamp
             , const std::string& type
-            , bool no_header
-            , const boost::optional< int >& quality
-            , bool do_index
-            , bool numbered
-            , bool force_filenames
+            , bool no_header = false
+            , const boost::optional< int >& quality = boost::none
+            , bool do_index = false
+            , bool numbered = false
+            , bool force_filenames = false
+            , bool exit_if_done = false
             , const std::vector< std::string >& filenames = std::vector< std::string >()
             , const std::vector< std::pair< unsigned int, unsigned int > >& ranges = std::vector< std::pair< unsigned int, unsigned int > >() );
 
         std::pair< H, cv::Mat > operator()( std::pair< H, cv::Mat > m );
+        
+        typedef boost::function< std::pair< H, cv::Mat >( std::pair< H, cv::Mat > ) > functor_t;
+        
+        static std::pair< functor_t, bool > make( boost::function< boost::posix_time::ptime( const H& ) > get_timestamp, const std::string& options );
+    
+        static std::string usage( unsigned int indent = 0 );
         
     private:
         const get_timestamp_functor get_timestamp_;
@@ -63,6 +44,7 @@ class file
         bool do_index_;
         bool numbered_;
         bool force_filenames_;
+        bool exit_if_done_;
         snark::cv_mat::serialization serialization_;
         boost::posix_time::ptime previous_timestamp_;
         unsigned int index_;
