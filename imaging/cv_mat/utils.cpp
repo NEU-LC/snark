6f520b4fe1711d8ac0c220581830a0518f280f25
vlaskine <vsevolod.vlaskine@gmail.com>
2020-11-20 10:30:27 +1100

    imaging: support for lower case for color conversions added

--- imaging/cv_mat/utils.cpp
+++ imaging/cv_mat/utils.cpp
@@ -1,39 +1,13 @@
-// This file is part of snark, a generic and flexible library for robotics research
 // Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <memory>
 #include <string>
-#include <comma/base/exception.h>
+#include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
+#include <comma/base/exception.h>
 #include <comma/base/types.h>
 #include "../../timing/time.h"
 #include "utils.h"
@@ -108,7 +82,7 @@ boost::unordered_map< int, std::string > fill_types_as_string_()
     return types;
 }
 
-boost::unordered_map< std::string, unsigned int > fill_cvt_color_types_()
+boost::unordered_map< std::string, unsigned int > fill_cvt_color_types_() // todo: split on comma, otherwise quadratic complexity
 {
     boost::unordered_map<std::string, unsigned int> types;
     //note RGB is exactly the same as BGR
@@ -148,13 +122,16 @@ cv::Scalar scalar_from_strings( const std::string* begin, unsigned int size )
     COMMA_THROW( comma::exception, "expected a scalar of the size up to 4, got: " << size << " elements" );
 }
 
-unsigned int cvt_color_type_from_string( const std::string& t ) // to avoid compilation warning
+unsigned int cvt_color_type_from_string( const std::string& t )
 {
     static boost::unordered_map< std::string, unsigned int > cvt_color_types_ = fill_cvt_color_types_();
     boost::unordered_map< std::string, unsigned int >::const_iterator it = cvt_color_types_.find( t );
     if( it != cvt_color_types_.end() ) { return it->second; }
-    try { return boost::lexical_cast< int >( t ); }
-    catch( ... ) {}
+    std::string s = t;
+    boost::to_upper( s );
+    it = cvt_color_types_.find( s ); // quick and dirty, until split on comma trivially implemented fill_cvt_color_types_
+    if( it != cvt_color_types_.end() ) { return it->second; }
+    try { return boost::lexical_cast< int >( t ); } catch( ... ) {}
     COMMA_THROW( comma::exception, "unknown conversion enum '" << t << "' for convert-color" );
 }
 
