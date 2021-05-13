44be19d10523a2feb222b499ac2176f16c17d994
Dave Jennings <dave.jennings@missionsystems.com.au>
2021-05-13 16:56:11 +1000

    advanced-navigation-cat: add support for external ntrip
    This should bring this application in to feature parity with the device
    connection side of advanced-navigation-spatial-to-csv.
    
    advanced-navigation-to-csv should cover the other features.

--- navigation/advanced-navigation/applications/advanced-navigation-cat.cpp
+++ navigation/advanced-navigation/applications/advanced-navigation-cat.cpp
@@ -7,6 +7,8 @@
 #include <comma/application/verbose.h>
 #include <comma/csv/stream.h>
 #include <comma/io/select.h>
+#include <tbb/concurrent_queue.h>
+#include <thread>
 
 namespace messages = snark::navigation::advanced_navigation::messages;
 
@@ -29,8 +31,11 @@ void usage( bool verbose )
     std::cerr << "\n    --flush:                flush output stream after each record";
     std::cerr << "\n    --input-fields:         print input command fields for --send and exit";
     std::cerr << "\n    --input-format:         print input command format for --send and exit";
+    std::cerr << "\n    --ntrip=<stream>:       read ntrip data from stream and send it to device";
     std::cerr << "\n    --send=<command>        read data from stdin and send as command packet";
     std::cerr << "\n    --sleep=<microseconds>: sleep between reading, default " << default_sleep;
+    std::cerr << "\n";
+    std::cerr << "\n    <stream> can be \"-\" for stdin; or a filename or \"tcp:<host>:<port>\" etc";
     std::cerr << "\n";
     std::cerr << "\n    --send: read commands from stdin and write command message to device";
     std::cerr << "\n            csv options apply to input stream";
@@ -72,6 +77,88 @@ static void bash_completion()
               << std::endl;
 }
 
+class ntrip
+{
+public:
+    typedef std::shared_ptr< std::vector< char > > item_t;
+
+    ntrip( const std::string& name )
+        : is( name,comma::io::mode::binary, comma::io::mode::non_blocking )
+        , buf( 4096 )
+    {
+        select.read().add( is.fd() );
+    }
+
+    bool process()
+    {
+        if( !is->good() ) return false;
+        unsigned size = is.available_on_file_descriptor();
+        if( !size ) { select.wait( boost::posix_time::microseconds( 20000 )); }
+        if( size || select.read().ready( is.fd() ))
+        {
+            //or use size = readsome...
+            is->read( &buf[0],size );
+            size = is->gcount();
+            if( size )
+            {
+                //put it in queue
+                queue.push( item_t( new std::vector< char >( buf.begin(), buf.begin()+size )));
+            }
+        }
+        return true;
+    }
+
+    static tbb::concurrent_bounded_queue< item_t > queue;
+
+private:
+    //create stream from name
+    comma::io::istream is;
+    comma::io::select select;
+    //app_t can check the queue and write to gps, instead of sleep
+    std::vector< char > buf;
+};
+
+//make a thread for reading
+class ntrip_thread
+{
+public:
+    ntrip_thread( const std::string& filename )
+        : shutdown( false )
+        , thread( &ntrip_thread::run, this )
+        , filename( filename )
+    {}
+
+    ~ntrip_thread()
+    {
+        shutdown = true;
+        thread.join();
+    }
+
+    void run()
+    {
+        while( !shutdown )
+        {
+            try
+            {
+                ntrip nt( filename );
+                while( !shutdown&&nt.process() ) {}
+            }
+            catch( std::exception& ex ) { std::cerr << comma::verbose.app_name() << ": " << ex.what() << std::endl; }
+            catch( ... ) { std::cerr << comma::verbose.app_name() << ": " << "unknown exception" << std::endl; }
+            //try to connect again in 3 seconds
+            for( unsigned i = 0; i < 100 && !shutdown; i++ )
+            {
+                std::this_thread::sleep_for( std::chrono::microseconds( 30 ));
+            }
+        }
+    }
+
+private:
+    bool shutdown;
+    std::thread thread;
+    std::string filename;
+};
+
 class app_base : protected snark::navigation::advanced_navigation::device
 {
 public:
@@ -89,6 +176,12 @@ public:
         {
             select.wait( boost::posix_time::microseconds( us ));
             if( !signal && select.read().ready( fd() )) { device::process(); }
+            if( !ntrip::queue.empty() )
+            {
+                ntrip::item_t item;
+                ntrip::queue.pop( item );
+                if( item ) { send_ntrip( *item ); }
+            }
         }
     }
 
@@ -183,6 +276,8 @@ struct send_app : protected snark::navigation::advanced_navigation::device
     }
 };
 
+tbb::concurrent_bounded_queue< ntrip::item_t > ntrip::queue;
+
 int main( int argc, char** argv )
 {
     try
@@ -205,6 +300,10 @@ int main( int argc, char** argv )
 
             return sf->run( options );
         }
+
+        std::unique_ptr< ntrip_thread > ntt;
+        auto opt_ntrip = options.optional< std::string >( "--ntrip" );
+        if( opt_ntrip ) { ntt.reset( new ntrip_thread( *opt_ntrip )); }
 
         app_raw( options ).process();
 
